5aa86d431149818a9e3a2587fa0d1a5d87e37523
Christopher Hilgenberg <chilgenb@icarusgpvm01.fnal.gov>
2018-09-09 12:42:01 -0500

    include trackID provided by AuxDetSimChannels for truth matching

--- icaruscode/CRT/CRTProducts/CRTChannelData.cxx
+++ icaruscode/CRT/CRTProducts/CRTChannelData.cxx
@@ -4,7 +4,7 @@ namespace icarus{
  namespace crt{
 
     CRTChannelData::CRTChannelData() : fChannel(0), fT0(0), fT1(0), fAdc(0), fTrackID(0) {}
-    CRTChannelData::CRTChannelData(uint32_t chan, uint32_t time0, uint32_t time1, uint32_t q, uint32_t trackid):
+    CRTChannelData::CRTChannelData(uint32_t chan, int time0, int time1, uint32_t q, int trackid):
       fChannel(chan),
       fT0(time0),
       fT1(time1),
@@ -14,16 +14,16 @@ namespace icarus{
     uint32_t CRTChannelData::Channel() const { 
       return fChannel; 
     }
-    uint32_t CRTChannelData::T0() const {
+    int CRTChannelData::T0() const {
       return fT0;
     }
-    uint32_t CRTChannelData::T1() const { 
+    int CRTChannelData::T1() const { 
       return fT1;
     }
     uint32_t CRTChannelData::ADC() const {
       return fAdc;
     }
-    uint32_t CRTChannelData::TrackID() const {
+    int CRTChannelData::TrackID() const {
       return fTrackID;
     }
     void CRTChannelData::SetADC(uint32_t adc)  {
--- icaruscode/CRT/CRTProducts/CRTChannelData.h
+++ icaruscode/CRT/CRTProducts/CRTChannelData.h
@@ -10,21 +10,21 @@ namespace crt {
 
     public:
       CRTChannelData();
-      CRTChannelData(uint32_t chan, uint32_t time0, uint32_t time1, uint32_t q, uint32_t trackid);
+      CRTChannelData(uint32_t chan, int time0, int time1, uint32_t q, int trackid);
       virtual ~CRTChannelData();
       uint32_t Channel() const;
-      uint32_t T0() const;
-      uint32_t T1() const;
+      int T0() const;
+      int T1() const;
       uint32_t ADC() const;
-      uint32_t TrackID() const;
+      int TrackID() const;
       void SetADC(uint32_t adc);
 
     private:
       uint32_t fChannel;
-      uint32_t fT0;
-      uint32_t fT1;
+      int fT0;
+      int fT1;
       uint32_t fAdc;
-      uint32_t fTrackID;
+      int fTrackID;
   };
 
  }
