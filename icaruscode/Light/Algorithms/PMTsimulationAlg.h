7146e9258cd060fc0cee7a6c5b7a5fcba6080896
Gianluca Petrillo <petrillo@slac.stanford.edu>
2018-10-23 13:16:09 -0500

    Further factorisation, and restructuring the code for the template 1 p.e. waveform

--- icaruscode/Light/Algorithms/PMTsimulationAlg.h
+++ icaruscode/Light/Algorithms/PMTsimulationAlg.h
@@ -43,15 +43,175 @@
 #include <cstdlib> // std::size_t
 
 
-namespace {
-  template <typename T>
-  T sqr(T v) { return v*v; }
-} // local namespace
-
 namespace icarus {
   namespace opdet {
     
+    // -------------------------------------------------------------------------
+    /// Utility class to make a number become a unique type.
+    template <typename T>
+    struct Value {
+      using Datum_t = T;
+      
+      constexpr Value(Datum_t v): fValue(v) {}
+      
+      operator Datum_t() const { return fValue; }
+      
+        private:
+      Datum_t fValue;
+    }; // class Value<>
+    
+    
+    // -------------------------------------------------------------------------
     /**
+     * @brief Describes the waveform from a single photoelectron.
+     * 
+     * This functor (class behaving like a function) describes the shape of the response
+     * to a single photoelectron.
+     *
+     * It is currently implemented as an asymmetric Gaussian shape.
+     */
+    class PhotoelectronPulseWaveform {
+      
+        public:
+     
+      /**
+       * @brief Constructor: assigns the parameters of the shape.
+       * @brief amplitude the maximum amplitudes of the shape (at transition)
+       * @brief peakTime the time of the maximum amplitude of the shape
+       * @brief sigmaLeft the standard deviation of the shape before transition
+       * @brief sigmaRight the standard deviation of the shape after transition
+       *
+       * The time parameters (`peakTime`, `sigmaLeft` and `sigmaRight`) must be
+       * measured in same unit. The `peakTime` defined the position of the shape
+       * with respect to time 0.
+       *
+       */
+      PhotoelectronPulseWaveform(
+        double amplitude,
+        double peakTime,
+        double sigmaLeft,
+        double sigmaRight
+        )
+        : fAmplitude(amplitude)
+        , fTransitTime(peakTime)
+        , fSigmaL(sigmaLeft)
+        , fSigmaR(sigmaRight)
+        {}
+      
+      // @{
+      // @name Parameter accessors.
+
+      double peakTime() const { return fTransitTime; }
+      double leftSigma() const { return fSigmaL; }
+      double rightSigma() const { return fSigmaR; }
+      double amplitude() const { return fAmplitude; }
+      
+      /// @}
+       
+      /**
+       * @brief Evaluates the pulse at the given time.
+       * @param time time to evaluate the shape at
+       * @see `PhotoelectronPulseWaveform()`
+       *
+       * The unit and scale of the time are defined by the transition time passed
+       * at construction.
+       */
+      double evaluateAt(double time) const
+        {
+          return Gaussian(time,
+            peakTime(), ((time < peakTime())? leftSigma(): rightSigma()),
+            amplitude()
+            );
+        } // evaluateAt()
+      
+      /// Alias of `evaluateAt()`.
+      double operator() (double time) const { return evaluateAt(time); }
+      
+      /// Returns the value of normal distribution at specified point.
+      static double Gaussian(double x, double mean, double sigma, double amplitude)
+        { return amplitude * std::exp(-sqr((x - mean)/sigma)/2.0); }
+      
+        private: 
+      double fAmplitude; ///< Amplitude of the Gaussian shapes at peak (transition).
+      double fTransitTime; ///< Time of transition between the two forms of shape.
+      double fSigmaL; ///< RMS parameter of the shape before transition.
+      double fSigmaR; ///< RMS parameter of the shape after transition.
+      
+      template <typename T>
+      static constexpr T sqr(T value) { return value * value; }
+      
+    }; // class PhotoelectronPulseWaveform
+ 
+
+    // -------------------------------------------------------------------------
+    /**
+     * @brief Precomputed digitised shape of a given function.
+     * @tparam T type of the samples being stored
+     *
+     * The sampling happens internally in double precision.
+     */
+    template <typename T>
+    class DiscretePhotoelectronPulse {
+        public:
+      using PulseFunction_t = PhotoelectronPulseWaveform; ///< Type of shape.
+      
+      struct Time_t: public Value<double> {};
+      struct Tick_t: public Value<std::size_t> {};
+      
+      /**
+       * @brief Constructor: samples the pulse.
+       * @param pulseShape the shape to be pulsed
+       * @param samplingFreq frequency of samples
+       *                     [inverse of time unit in `pulseSape`]
+       * @param rightSigmas sample until this standard deviations after peak
+       *
+       * Samples start from time 0 (as defined by the pulse shape).
+       */
+      DiscretePhotoelectronPulse
+        (PulseFunction_t&& pulseShape, double samplingFreq, double rightSigmas);
+      
+      /// Returns the length of the sampled pulse in ticks.
+      std::size_t pulseLength() const { return fSampledShape.size(); }
+      
+      /// Returns the value sampled for the specified `tick` (the first is `0`).
+      T operator[] (std::size_t tick) const { return fSampledShape[tick]; }
+      
+      /// Returns the value sampled for the specified `tick` (the first is `0`).
+      T operator() (Tick_t tick) const { return this->operator[](tick); }
+      
+      /// Evaluates the shape at the specified time.
+      T operator() (Time_t time) const { return fShape(time); }
+      
+      /// @{
+      /// @name Iterator interface.
+
+      auto cbegin() const { return fSampledShape.cbegin(); }
+      auto cend() const { return fSampledShape.cend(); }
+      auto begin() const { return fSampledShape.begin(); }
+      auto end() const { return fSampledShape.end(); }
+      
+      /// @}
+      
+      /// Returns the function which was sampled.
+      PulseFunction_t const& shape() const { return fShape; }
+      
+        private:
+      PulseFunction_t fShape; ///< Analytical shape of the pules.
+      double fSamplingFreq;   ///< Sampling frequency.
+      
+      std::vector<T> const fSampledShape; ///< Pulse shape, discretised.
+      
+      /// Builds the sampling cache.
+      static std::vector<T> sampleShape(
+        PulseFunction_t const& pulseShape,
+        double samplingFreq, double rightSigmas
+        );
+      
+    }; // class DiscretePhotoelectronPulse<>
+    
+    // -------------------------------------------------------------------------
+
+    /** ************************************************************************
      * @brief Algorithm class for the full simulation of PMT channels.
      *
      *
@@ -59,40 +219,64 @@ namespace icarus {
     class PMTsimulationAlg {
       
         public:
-      
-      struct Config {
-        using Name = fhicl::Name;
-        using Comment = fhicl::Comment;
+    
+      /// Type holding all configuration parameters for this algorithm.
+      struct ConfigurationParameters_t {    
         
-        fhicl::Atom<double> ReadoutEnablePeriod {
-          Name("ReadoutEnablePeriod"),
-          Comment("Time for which PMT readout is enabled [us]")
-          };
+        /// @{
+        /// @name High level configuration parameters.
+    
+        double QEbase;         ///< Uncorrected PMT quantum efficiency.
         
-      }; // struct Config
+        size_t readoutWindowSize;     ///ReadoutWindowSize in samples
+        float  pretrigFraction;       ///Fraction of window size to be before "trigger"
+        float  thresholdADC;          ///ADC Threshold for self-triggered readout
+        int    pulsePolarity;         ///Pulse polarity (=1 for positive, =-1 for negative)
+        double triggerOffsetPMT;      ///Time (us) relative to trigger when pmt readout starts
+            
+        double  readoutEnablePeriod;  ///Time (us) for which pmt readout is enabled
+    
+        bool createBeamGateTriggers; ///Option to create unbiased readout around beam spill
+        double beamGateTriggerRepPeriod; ///Repetition Period (us) for BeamGateTriggers
+        size_t beamGateTriggerNReps; ///Number of beamgate trigger reps to produce
+    
+        double ADC;      //charge to ADC convertion scale
+        double transitTime;   //to be added to pulse minimum time
+        double meanAmplitude;  //in pC
+        double fallTime; //in ns
+        double riseTime; //in ns
+    
+        double baseline; //waveform baseline    double fAmpNoise; //amplitude of gaussian noise
+        double ampNoise; //amplitude of gaussian noise
+        double darkNoiseRate; //in Hz
+        double saturation; //equivalent to the number of p.e. that saturates the electronic signal	
+        /// @}
+        
+        /// @{
+        /// @name Setup parameters
+
+        
+        detinfo::LArProperties const* larProp = nullptr; ///< LarProperties service provider.
+        detinfo::DetectorClocks const* timeService = nullptr; ///< DetectorClocks service provider.
+        CLHEP::HepRandomEngine* randomEngine = nullptr; ///< Main random stream engine.
+        CLHEP::HepRandomEngine* darkNoiseRandomEngine = nullptr; ///< Dark noise random stream engine.
+        CLHEP::HepRandomEngine* elecNoiseRandomEngine = nullptr; ///< Electronics noise random stream engine.
+        /// @}
+
+        /// @{
+        /// @name Derivative configuration parameters.
+        std::size_t pretrigSize() const { return pretrigFraction * readoutWindowSize; }
+        std::size_t posttrigSize() const { return readoutWindowSize - pretrigSize(); }
+        
+        /// @}
+        
+      }; // ConfigurationParameters_t
+
+
       
       /// Constructor.
-      PMTsimulationAlg(Config const& config); 
-      PMTsimulationAlg(fhicl::ParameterSet const& pset); 
+      PMTsimulationAlg(ConfigurationParameters_t const& config); 
       
-      /**
-       * @brief Set up for the next event.
-       * @param larProp instance of `detinfo::LArProperties` to be used
-       * @param detClocks instance of `detinfo::DetectorClocks` to be used
-       * @param mainRandomEngine main random engine (quantum efficiency, etc.)
-       * @param darkNoiseRandomEngine random engine for dark noise simulation
-       * @param elecNoiseRandomEngine random engine for electronics noise simulation
-       *
-       * All random engines are required in this interface, even if the
-       * configuration disabled noise simulation.
-       */
-      void setup(
-        detinfo::LArProperties const& larProp,
-        detinfo::DetectorClocks const& detClocks,
-        CLHEP::HepRandomEngine& mainRandomEngine, 
-        CLHEP::HepRandomEngine& darkNoiseRandomEngine, 
-        CLHEP::HepRandomEngine& elecNoiseRandomEngine 
-        );    
       
       /**
        * @brief Returns the waveforms originating from simulated photons.
@@ -113,6 +297,13 @@ namespace icarus {
       /// Type internally used for storing waveforms.
       using Waveform_t = std::vector<float>;
       
+      ConfigurationParameters_t fParams; ///< Complete algorithm configuration.
+    
+      double fQE;            ///< PMT quantum efficiency.
+      double fSampling;      ///< Wave sampling frequency [MHz].
+      std::size_t fNsamples; ///< Samples per waveform.
+
+      DiscretePhotoelectronPulse<float> wsp; /// Single photon pulse (sampled).
     
     void CreateFullWaveform
       (Waveform_t&, std::vector<unsigned int>&, sim::SimPhotons const&);
@@ -120,53 +311,13 @@ namespace icarus {
     void CreateOpDetWaveforms(raw::Channel_t const& opch,
 			      Waveform_t const& wvfm,
 			      std::vector<raw::OpDetWaveform>& output_opdets);
-    
-    double fSampling;       //wave sampling frequency (GHz)
-    std::size_t fNsamples; //Samples per waveform
 
-    double fQEbase;         ///< Uncorrected PMT quantum efficiency.
-    double fQE;             //PMT quantum efficiency
-    
-    size_t fReadoutWindowSize;     ///ReadoutWindowSize in samples
-    float  fPretrigFraction;       ///Fraction of window size to be before "trigger"
-    float  fThresholdADC;          ///ADC Threshold for self-triggered readout
-    int    fPulsePolarity;         ///Pulse polarity (=1 for positive, =-1 for negative)
-    double  fTriggerOffsetPMT;      ///Time (us) relative to trigger when pmt readout starts
-        
-    double  fReadoutEnablePeriod;  ///Time (us) for which pmt readout is enabled
-
-    size_t fPretrigSize;
-    size_t fPosttrigSize;
-// 
-    bool fCreateBeamGateTriggers; ///Option to create unbiased readout around beam spill
-    double fBeamGateTriggerRepPeriod; ///Repetition Period (us) for BeamGateTriggers
-    size_t fBeamGateTriggerNReps; ///Number of beamgate trigger reps to produce
-// 
-//     //Single PE parameters
-//     double fFallTime;       //fall time of 1PE in ns
-//     double fRiseTime;      //rise time in ns
-    double fTransitTime;   //to be added to pulse minimum time
-    double sigma1;
-    double sigma2;
-    double fMeanAmplitude;  //in pC
-    double fFallTime; //in ns
-    double fRiseTime; //in ns
 
     void AddSPE(size_t time_bin, Waveform_t& wave); // add single pulse to auxiliary waveform
     /// Add `n` standard pulses starting at the specified `time_bin` of `wave`.
     void AddPhotoelectrons(size_t time_bin, unsigned int n, Waveform_t& wave) const;
 
-    double Pulse1PE(double time) const;
     
-    Waveform_t wsp; //single photon pulse vector
-    
-    int pulsesize; //size of 1PE waveform
-    
-    double fADC;      //charge to ADC convertion scale
-    double fBaseline; //waveform baseline
-    double fAmpNoise; //amplitude of gaussian noise
-    double fDarkNoiseRate; //in Hz
-    double fSaturation; //equivalent to the number of p.e. that saturates the electronic signal	
     
     void AddNoise(Waveform_t& wave); //add noise to baseline
     void AddDarkNoise(Waveform_t& wave); //add noise to baseline
@@ -174,14 +325,61 @@ namespace icarus {
     std::set<size_t> CreateBeamGateTriggers() const;
     std::set<size_t> FindTriggers(Waveform_t const& wvfm) const;
 
-    detinfo::DetectorClocks const* fTimeService = nullptr; ///< DetectorClocks service provider.
-    CLHEP::HepRandomEngine* fRandomEngine = nullptr; ///< Main random stream engine.
-    CLHEP::HepRandomEngine* fDarkNoiseRandomEngine = nullptr; ///< Dark noise random stream engine.
-    CLHEP::HepRandomEngine* fElecNoiseRandomEngine = nullptr; ///< Electronics noise random stream engine.
     
       /// Returns a random response whether a photon generates a photoelectron.
       bool KicksPhotoelectron() const;
     }; // class PMTsimulationAlg
+    
+    
+    // -------------------------------------------------------------------------
+    
+    /// Returns a new `PMTsimulationAlg` with an updated configuration.
+    class PMTsimulationAlgMaker {
+      
+         public:
+      struct Config {
+        using Name = fhicl::Name;
+        using Comment = fhicl::Comment;
+        
+        fhicl::Atom<double> ReadoutEnablePeriod {
+          Name("ReadoutEnablePeriod"),
+          Comment("Time for which PMT readout is enabled [us]")
+          };
+        
+      }; // struct Config
+      
+      /// Constructor.
+      PMTsimulationAlgMaker(Config const& config); 
+      PMTsimulationAlgMaker(fhicl::ParameterSet const& pset); 
+      
+      /**
+       * @brief Creates and returns a new algorithm instance.
+       * @param larProp instance of `detinfo::LArProperties` to be used
+       * @param detClocks instance of `detinfo::DetectorClocks` to be used
+       * @param mainRandomEngine main random engine (quantum efficiency, etc.)
+       * @param darkNoiseRandomEngine random engine for dark noise simulation
+       * @param elecNoiseRandomEngine random engine for electronics noise simulation
+       *
+       * All random engines are required in this interface, even if the
+       * configuration disabled noise simulation.
+       */
+      std::unique_ptr<PMTsimulationAlg> operator()(
+        detinfo::LArProperties const& larProp,
+        detinfo::DetectorClocks const& detClocks,
+        CLHEP::HepRandomEngine& mainRandomEngine, 
+        CLHEP::HepRandomEngine& darkNoiseRandomEngine, 
+        CLHEP::HepRandomEngine& elecNoiseRandomEngine
+        ) const;
+       
+        private:
+      /// Part of the configuration learned from configuration files.
+      PMTsimulationAlg::ConfigurationParameters_t fBaseConfig;
+       
+    }; // class PMTsimulationAlgMaker
+    
+    
+    // -------------------------------------------------------------------------
+    
   
   } // namespace opdet
 } // namespace icarus
@@ -193,17 +391,19 @@ void icarus::opdet::PMTsimulationAlg::printConfiguration
   (Stream&& out, std::string indent /* = "" */) const
 {
   out
-            << indent << "Baseline:          " << fBaseline << " ADC"
-    << '\n' << indent << "ReadoutWindowSize: " << fReadoutWindowSize << " ticks"
-    << '\n' << indent << "PreTrigFraction:   " << fPretrigFraction
-    << '\n' << indent << "ThresholdADC:      " << fThresholdADC << " ADC"
-    << '\n' << indent << "Saturation:        " << fSaturation << " ADC"
-    << '\n' << indent << "PulsePolarity:     " << ((fPulsePolarity == 1)? "positive": "negative") << " (=" << fPulsePolarity << ")"
+            << indent << "Baseline:          " << fParams.baseline << " ADC"
+    << '\n' << indent << "ReadoutWindowSize: " << fParams.readoutWindowSize << " ticks"
+    << '\n' << indent << "PreTrigFraction:   " << fParams.pretrigFraction
+    << '\n' << indent << "ThresholdADC:      " << fParams.thresholdADC << " ADC"
+    << '\n' << indent << "Saturation:        " << fParams.saturation << " ADC"
+    << '\n' << indent << "PulsePolarity:     " << ((fParams.pulsePolarity == 1)? "positive": "negative") << " (=" << fParams.pulsePolarity << ")"
     << '\n' << indent << "Sampling:          " << fSampling << " MHz"
     << '\n' << indent << "Samples/waveform:  " << fNsamples << " ticks"
     ;
-  if (fCreateBeamGateTriggers) 
-    out << '\n' << indent << "Create " << fBeamGateTriggerNReps << " beam gate triggers, one every " << fBeamGateTriggerRepPeriod << " us.";
+  if (fParams.createBeamGateTriggers) {
+    out << '\n' << indent << "Create " << fParams.beamGateTriggerNReps
+      << " beam gate triggers, one every " << fParams.beamGateTriggerRepPeriod << " us.";
+  }
   else out << '\n' << indent << "Do not create beam gate triggers.";
   
   
