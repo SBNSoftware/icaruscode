6af5268ba2d8bde5087160b27aabaa1e81ab96a3
Lynn Garren <garren@fnal.gov>
2018-12-11 21:47:21 -0600

    add missing header

--- icaruscode/RawDigitFilter/RawDigitSmoother_module.cc
+++ icaruscode/RawDigitFilter/RawDigitSmoother_module.cc
@@ -23,6 +23,7 @@
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h" 
 #include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Principal/Event.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Utilities/make_tool.h"
 #include "canvas/Persistency/Common/Ptr.h"
