3f4f34baf3fd359cdcefdf2869c56bd64db85644
Christopher Hilgenberg <chilgenb@icarusgpvm01.fnal.gov>
2019-02-15 16:50:34 -0600

    change to same CRTHit products used by uBooNE and SBND

--- icaruscode/CRT/CRTProducts/classes.h
+++ icaruscode/CRT/CRTProducts/classes.h
@@ -1,7 +1,7 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "icaruscode/CRT/CRTProducts/CRTChannelData.h"
 #include "icaruscode/CRT/CRTProducts/CRTData.hh"
-#include "icaruscode/CRT/CRTProducts/CRTHit.h"
+#include "icaruscode/CRT/CRTProducts/CRTHit.hh"
 #include <vector>
 //#include <utility>
 //#include <cstdint>
