d0263e2beeb92c90d02f2b40870ce454e17f28ea
Christopher Hilgenberg <Chris.Hilgenberg@colostate.edu>
2019-07-25 11:15:58 -0500

    changes for compatibility with new CRT geometry

--- icaruscode/CRT/CRTGeoObjectSorter.cxx
+++ icaruscode/CRT/CRTGeoObjectSorter.cxx
@@ -13,8 +13,6 @@
 #include "larcorealg/Geometry/AuxDetGeo.h"
 #include "larcorealg/Geometry/AuxDetSensitiveGeo.h"
 
-//namespace icarus{
-//namespace crt {
 namespace geo{
 
   //----------------------------------------------------------------------------
@@ -24,11 +22,11 @@ namespace geo{
     // sort based off of GDML name, module number
     std::string ad1name = (ad1->TotalVolume())->GetName();
     std::string ad2name = (ad2->TotalVolume())->GetName();
-    // assume volume name is "volAuxDet_Module_###_<region>"
-    std::string base = "volAuxDet_Module_";
+    // assume volume name is "volAuxDet_<subsystem>_module_###_<region>"
+    std::string modulePrefix = "module_";
 
-    int ad1Num = atoi( ad1name.substr( base.size(), 3).c_str() );
-    int ad2Num = atoi( ad2name.substr( base.size(), 3).c_str() );
+    int ad1Num = atoi( ad1name.substr(ad1name.find(modulePrefix)+modulePrefix.length(),3).c_str() );
+    int ad2Num = atoi( ad2name.substr(ad2name.find(modulePrefix)+modulePrefix.length(), 3).c_str() );
     
     return ad1Num < ad2Num;
   }
@@ -41,17 +39,17 @@ namespace geo{
     // sort based off of GDML name, assuming ordering is encoded
     std::string ad1name = (ad1->TotalVolume())->GetName();
     std::string ad2name = (ad2->TotalVolume())->GetName();
-    // assume volume name is "volAuxDetSensitive_Module_###_Strip_##"
-    std::string baseMod = "volAuxDetSensitive_Module_";
-    std::string baseStr = "volAuxDetSensitive_Module_###_Strip_";
+    // assume volume name is "volAuxDetSensitive_<subsystem>_module_###_(<cut<###>,top or bot>_)strip_##"
+    std::string modulePrefix = "module_";
+    std::string stripPrefix = "strip_";
 
-    int ad1Num = atoi( ad1name.substr( baseMod.size(), 3).c_str() );
-    int ad2Num = atoi( ad2name.substr( baseMod.size(), 3).c_str() );
+    int ad1Num = atoi( ad1name.substr(ad1name.find(modulePrefix)+modulePrefix.length(), 3).c_str() );
+    int ad2Num = atoi( ad2name.substr(ad2name.find(modulePrefix)+modulePrefix.length(), 3).c_str() );
 
     if(ad1Num!=ad2Num) return ad1Num < ad2Num; 
 
-    ad1Num = atoi( ad1name.substr( baseStr.size(), 2).c_str() );
-    ad2Num = atoi( ad2name.substr( baseStr.size(), 2).c_str() );
+    ad1Num = atoi( ad1name.substr(ad1name.find(stripPrefix)+stripPrefix.length(), 2).c_str() );
+    ad2Num = atoi( ad2name.substr((ad2name.find(stripPrefix)+stripPrefix.length(), 2).c_str() );
 
     return ad1Num < ad2Num;
 
