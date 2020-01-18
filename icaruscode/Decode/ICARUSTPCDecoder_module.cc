730d913a5d5acda9bee681ff47a537d269b04e28
Tracy Usher <usher@slac.stanford.edu>
2020-01-18 09:52:48 -0800

    Sorting out the ad hoc channel number based simply on fragment, board and index.

--- icaruscode/Decode/ICARUSTPCDecoder_module.cc
+++ icaruscode/Decode/ICARUSTPCDecoder_module.cc
@@ -25,120 +25,145 @@
 #include "art_root_io/TFileService.h" 
 #include "art_root_io/TFileDirectory.h" 
 
+namespace daq
+{
 
 DEFINE_ART_MODULE(daq::ICARUSTPCDecoder)
-daq::ICARUSTPCDecoder::ICARUSTPCDecoder(fhicl::ParameterSet const & param): 
-  art::EDProducer{param},
-  _tag(param.get<std::string>("raw_data_label", "daq"),param.get<std::string>("fragment_type_label", "PHYSCRATEDATA")),
-  _config(param),
-  _last_event_number(0),
-  _last_trig_frame_number(0),
-  _fragment_id_offset(param.get<uint32_t>("fragment_id_offset"))
- {
-  
-  // produce stuff
-  produces<std::vector<raw::RawDigit>>();
- // if (_config.produce_header) {
- //   produces<std::vector<tpcAnalysis::HeaderData>>();
- // }
-
-  art::ServiceHandle<art::TFileService> tfs;
-  _header_ana_tree = tfs->make<TTree>("_header_ana_tree","Header Ana Tree");
-
-  _header_ana_tree->Branch("fragment_id",&_fragment_id,"fragment_id/i");
-  _header_ana_tree->Branch("board_id",&_board_id,"board_id/i");
-  _header_ana_tree->Branch("event",&_event_number,"event/i");
-  _header_ana_tree->Branch("timestamp",&_timestamp,"timestamp/i");
-
-}
-daq::ICARUSTPCDecoder::Config::Config(fhicl::ParameterSet const & param) {
-  // amount of time to wait in between processing events
-  // useful for debugging redis
-  double wait_time = param.get<double>("wait_time", -1 /* units of seconds */);
-  wait_sec = (int) wait_time;
-  wait_usec = (int) (wait_time / 1000000.);
-  // whether to calcualte the pedestal (and set it in SetPedestal())
-  baseline_calc = param.get<bool>("baseline_calc", false);
-  // whether to put headerinfo in the art root file
-  produce_header = param.get<bool>("produce_header", false);
-  // how many adc values to skip in mode/pedestal finding
-  n_mode_skip = param.get<unsigned>("n_mode_skip", 1);
-  // whether to subtract pedestal
-  subtract_pedestal = param.get<bool>("subtract_pedestal", false);
-  // icarus readout window length
-  timesize = param.get<unsigned>("timesize", 1);
-  // icarus tick length (for timestamp)
-  // should be 1/(2.5MHz) = 0.4mus
-  frame_to_dt = param.get<double>("frame_to_dt", 1);
-  // number of channels in each slot
-  channel_per_slot = param.get<unsigned>("channel_per_slot", 0);
-  // index of 0th slot
-  min_slot_no = param.get<unsigned>("min_slot_no", 0);
-}
-void daq::ICARUSTPCDecoder::produce(art::Event & event)
+ICARUSTPCDecoder::ICARUSTPCDecoder(fhicl::ParameterSet const & param): art::EDProducer{param},
+    _tag(param.get<std::string>("raw_data_label", "daq"),param.get<std::string>("fragment_type_label", "PHYSCRATEDATA")),
+    _config(param),
+    _last_event_number(0),
+    _last_trig_frame_number(0),
+    _fragment_id_offset(param.get<uint32_t>("fragment_id_offset"))
 {
-  if (_config.wait_sec >= 0) {
-    std::this_thread::sleep_for(std::chrono::seconds(_config.wait_sec) + std::chrono::microseconds(_config.wait_usec));
-  }
-  auto const& daq_handle = event.getValidHandle<artdaq::Fragments>(_tag);
   
-  // storage for waveform
-  std::unique_ptr<std::vector<raw::RawDigit>> product_collection(new std::vector<raw::RawDigit>);
-  // storage for header info
- // std::unique_ptr<std::vector<tpcAnalysis::HeaderData>> header_collection(new std::vector<tpcAnalysis::HeaderData>);
-  for (auto const &rawfrag: *daq_handle) {
-    //process_fragment(event, rawfrag, product_collection, header_collection);
-process_fragment(event, rawfrag, product_collection);
-  }
-  event.put(std::move(product_collection));
-  //if (_config.produce_header) {
- //   event.put(std::move(header_collection));
- // }
+    // produce stuff
+    produces<std::vector<raw::RawDigit>>();
+    // if (_config.produce_header) {
+    //   produces<std::vector<tpcAnalysis::HeaderData>>();
+    // }
+
+    art::ServiceHandle<art::TFileService> tfs;
+    _header_ana_tree = tfs->make<TTree>("_header_ana_tree","Header Ana Tree");
+
+    _header_ana_tree->Branch("fragment_id",&_fragment_id,"fragment_id/i");
+    _header_ana_tree->Branch("board_id",&_board_id,"board_id/i");
+    _header_ana_tree->Branch("event",&_event_number,"event/i");
+    _header_ana_tree->Branch("timestamp",&_timestamp,"timestamp/i");
+
+    return;
+}
+ICARUSTPCDecoder::Config::Config(fhicl::ParameterSet const & param) 
+{
+    // amount of time to wait in between processing events
+    // useful for debugging redis
+    double wait_time = param.get<double>("wait_time", -1 /* units of seconds */);
+    wait_sec = (int) wait_time;
+    wait_usec = (int) (wait_time / 1000000.);
+
+    // whether to calcualte the pedestal (and set it in SetPedestal())
+    baseline_calc = param.get<bool>("baseline_calc", false);
+
+    // whether to put headerinfo in the art root file
+    produce_header = param.get<bool>("produce_header", false);
+
+    // how many adc values to skip in mode/pedestal finding
+    n_mode_skip = param.get<unsigned>("n_mode_skip", 1);
+
+    // whether to subtract pedestal
+    subtract_pedestal = param.get<bool>("subtract_pedestal", false);
+
+    // icarus readout window length
+    timesize = param.get<unsigned>("timesize", 1);
+
+    // icarus tick length (for timestamp)
+    // should be 1/(2.5MHz) = 0.4mus
+    frame_to_dt = param.get<double>("frame_to_dt", 1);
+
+    // number of channels in each slot
+    channel_per_slot = param.get<unsigned>("channel_per_slot", 0);
+
+    // index of 0th slot
+    min_slot_no = param.get<unsigned>("min_slot_no", 0);
+
+    return;
+}
+void ICARUSTPCDecoder::produce(art::Event & event)
+{
+    if (_config.wait_sec > 0)
+      std::this_thread::sleep_for(std::chrono::seconds(_config.wait_sec) + std::chrono::microseconds(_config.wait_usec));
+
+    auto const& daq_handle = event.getValidHandle<artdaq::Fragments>(_tag);
+    
+    // storage for waveform
+    std::unique_ptr<std::vector<raw::RawDigit>> product_collection(new std::vector<raw::RawDigit>);
+
+    std::cout << "**************** PROCESSING NEW EVENT **********************" << std::endl;
+    std::cout << "--> fragments size: " << daq_handle->size() << std::endl;
+
+    // storage for header info
+   // std::unique_ptr<std::vector<tpcAnalysis::HeaderData>> header_collection(new std::vector<tpcAnalysis::HeaderData>);
+    for (auto const &rawfrag: *daq_handle) 
+    {
+      //process_fragment(event, rawfrag, product_collection, header_collection);
+      process_fragment(event, rawfrag, product_collection);
+    }
+
+    std::cout << "================= DONE PROCESSING EVENT ==================" << std::endl;
+
+    event.put(std::move(product_collection));
+
+    return;
 }
 
 
-void daq::ICARUSTPCDecoder::process_fragment(art::Event &event, const artdaq::Fragment &frag, 
-  std::unique_ptr<std::vector<raw::RawDigit>> &product_collection) {
-
-
-  std::cout << "FragmentID is " << frag.fragmentID() << std::endl;
-  _fragment_id = frag.fragmentID() - _fragment_id_offset;
-
-  // convert fragment to Nevis fragment
-  icarus::PhysCrateFragment fragment(frag);
-  std::cout << " n boards " << fragment.nBoards() << std::endl;
+void ICARUSTPCDecoder::process_fragment(art::Event                                  &event, 
+                                        const artdaq::Fragment                      &frag, 
+                                        std::unique_ptr<std::vector<raw::RawDigit>> &product_collection) 
+{
+    _fragment_id = frag.fragmentID() - _fragment_id_offset;
   
-//int channel_count=0;
-  for(size_t i_b=0; i_b < fragment.nBoards(); i_b++){
+    // convert fragment to Nevis fragment
+    icarus::PhysCrateFragment fragment(frag);
 
-    _board_id = i_b;
-    _event_number = fragment.BoardEventNumber(i_b);
-    _timestamp = fragment.BoardTimeStamp(i_b);
+    std::cout << "---> FragmentID: " << frag.fragmentID() << ", _fragment_id: " << _fragment_id << ", # boards: " << fragment.nBoards() << std::endl;
+    
+    size_t nBoardsPerFragment = fragment.nBoards();
+    size_t nChannelsPerBoard  = fragment.nChannelsPerBoard();
 
-    _header_ana_tree->Fill();
+    //int channel_count=0;
+    for(size_t i_b=0; i_b < fragment.nBoards(); i_b++)
+    {
+        _board_id     = i_b;
+        _event_number = fragment.BoardEventNumber(i_b);
+        _timestamp    = fragment.BoardTimeStamp(i_b);
+  
+        _header_ana_tree->Fill();
 
-    //A2795DataBlock const& block_data = *(crate_data.BoardDataBlock(i_b));
-    for(size_t i_ch=0; i_ch < fragment.nChannelsPerBoard(); ++i_ch){
-      //raw::ChannelID_t channel_num = (i_ch & 0xff ) + (i_b << 8);
-      raw::ChannelID_t channel_num = i_ch+i_b*64+_fragment_id*5*64;
-      raw::RawDigit::ADCvector_t wvfm(fragment.nSamplesPerChannel());
-      for(size_t i_t=0; i_t < fragment.nSamplesPerChannel(); ++i_t) {
-	wvfm[i_t] = fragment.adc_val(i_b,i_ch,i_t);
-	// if(channel_num==1855) std::cout << " sample " << i_t << " wave " << wvfm[i_t] << std::endl;
-      }
-      //   product_collection->emplace_back(channel_count++,fragment.nSamplesPerChannel(),wvfm);
-      product_collection->emplace_back(channel_num,fragment.nSamplesPerChannel(),wvfm);
-      //std::cout << " channel " << channel_num << " waveform size " << fragment.nSamplesPerChannel() << std::endl;
-    }//loop over channels
-  }//loop over boards
-  std::cout << "Total number of channels added is " << product_collection->size() << std::endl;
+        std::cout << "    --> Processing board " << i_b << ", ev # " << _event_number << ", time: " << _timestamp 
+                  << ", # channels: " << fragment.nChannelsPerBoard() << ", # samples: " << fragment.nSamplesPerChannel() << std::endl;
 
+        size_t boardId = nChannelsPerBoard * (nBoardsPerFragment * _fragment_id + i_b);
 
+        //A2795DataBlock const& block_data = *(crate_data.BoardDataBlock(i_b));
+        for(size_t i_ch = 0; i_ch < fragment.nChannelsPerBoard(); ++i_ch)
+        {
+            //raw::ChannelID_t channel_num = (i_ch & 0xff ) + (i_b << 8);
+            raw::ChannelID_t           channel_num = boardId + i_ch;
+            raw::RawDigit::ADCvector_t wvfm(fragment.nSamplesPerChannel());
 
+            std::cout << "        --> i_ch: " << i_ch << ", boardId: " << boardId << ", chan: " << channel_num << std::endl;
 
+            for(size_t i_t=0; i_t < fragment.nSamplesPerChannel(); ++i_t) {
+  	            wvfm[i_t] = fragment.adc_val(i_b,i_ch,i_t);
+            }
+        
+            //   product_collection->emplace_back(channel_count++,fragment.nSamplesPerChannel(),wvfm);
+            product_collection->emplace_back(channel_num,fragment.nSamplesPerChannel(),wvfm);
+        }//loop over channels
+    }//loop over boards
 
-
-
+    std::cout << "Total number of channels added is " << product_collection->size() << std::endl;
 
   /*std::unordered_map<uint16_t,sbnddaq::NevisTPC_Data_t> waveform_map;
     size_t n_waveforms = fragment.decode_data(waveform_map);
@@ -185,6 +210,7 @@ void daq::ICARUSTPCDecoder::process_fragment(art::Event &event, const artdaq::Fr
     }
   }
 */
+    return;
 }
 // Computes the checksum, given a nevis tpc header
 // Ideally this would be in sbnddaq-datatypes, but it's not and I can't
@@ -208,4 +234,6 @@ uint32_t daq::ICARUSTPCDecoder::compute_checksum(icarus::PhysCrateFragment &frag
 
 }
 */
+
+} // end namespace
 
