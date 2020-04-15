812dc857be651fa9fb7c6b95705c25a8eedd31d6
Christopher Hilgenberg <Chris.Hilgenberg@colostate.edu>
2020-04-14 19:52:01 -0500

    fresh analyzer module from cetskelgen

---
+++ icaruscode/CRT/CrtOpHitMatchAnalysis_module.cc
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       CrtOpHitMatchAnalysis
+// Plugin Type: analyzer (art v3_05_00)
+// File:        CrtOpHitMatchAnalysis_module.cc
+//
+// Generated at Tue Apr 14 19:49:18 2020 by Christopher Hilgenberg using cetskelgen
+// from cetlib version v3_10_00.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "canvas/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "icaruscode/CRT/CRTProducts/CRTHit.hh"
+#include "icaruscode/"
+
+class CrtOpHitMatchAnalysis;
+
+
+class CrtOpHitMatchAnalysis : public art::EDAnalyzer {
+public:
+  explicit CrtOpHitMatchAnalysis(fhicl::ParameterSet const& p);
+  // The compiler-generated destructor is fine for non-base
+  // classes without bare pointers or other resource use.
+
+  // Plugins should not be copied or assigned.
+  CrtOpHitMatchAnalysis(CrtOpHitMatchAnalysis const&) = delete;
+  CrtOpHitMatchAnalysis(CrtOpHitMatchAnalysis&&) = delete;
+  CrtOpHitMatchAnalysis& operator=(CrtOpHitMatchAnalysis const&) = delete;
+  CrtOpHitMatchAnalysis& operator=(CrtOpHitMatchAnalysis&&) = delete;
+
+  // Required functions.
+  void analyze(art::Event const& e) override;
+
+  // Selected optional functions.
+  void beginJob() override;
+  void endJob() override;
+
+private:
+
+  // Declare member data here.
+
+};
+
+
+CrtOpHitMatchAnalysis::CrtOpHitMatchAnalysis(fhicl::ParameterSet const& p)
+  : EDAnalyzer{p}  // ,
+  // More initializers here.
+{
+  // Call appropriate consumes<>() for any products to be retrieved by this module.
+}
+
+void CrtOpHitMatchAnalysis::analyze(art::Event const& e)
+{
+  // Implementation of required member function here.
+}
+
+void CrtOpHitMatchAnalysis::beginJob()
+{
+  // Implementation of optional member function here.
+}
+
+void CrtOpHitMatchAnalysis::endJob()
+{
+  // Implementation of optional member function here.
+}
+
+DEFINE_ART_MODULE(CrtOpHitMatchAnalysis)
