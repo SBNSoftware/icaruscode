1e7a2a2af1cd433aac206ea7529273cfbc0127b9
Gianluca Petrillo <petrillo@slac.stanford.edu>
2020-04-21 21:30:23 -0500

    Documentation fixes.

--- icaruscode/PMT/SimPMTIcarus_module.cc
+++ icaruscode/PMT/SimPMTIcarus_module.cc
@@ -63,6 +63,8 @@ namespace icarus::opdet {
    * `icarus::opdet::PMTsimulationAlg` algorithm, with the following exceptions:
    * * **InputModule** (input tag): tag of the data product containing the
    *   `sim::SimPhotons` collection to be digitized;
+   * * **SinglePhotonResponse** (configuration table): configuration of the
+   *   _art_ tool delivering the single photon response function (see below);
    * * **EfficiencySeed**, **DarkNoiseSeed** and **ElectronicsNoiseSeed**
    *   (integers, optional): if specified, each number is used to seed the
    *   pertaining random engine; otherwise, the seed is assigned by
@@ -113,6 +115,30 @@ namespace icarus::opdet {
    * * `LArPropertiesService` for the scintillation yield(s)
    * 
    * Three random streams are also used.
+   * 
+   * 
+   * Single photon response function tool
+   * -------------------------------------
+   * 
+   * The single photon response function passed to the underlying algorithm,
+   * `icarus::opdet::PMTsimulationAlg`, is extracted from an _art_ tool.
+   * The specifications of the tool are:
+   * 
+   * * the _art_ tool class must implement the
+   *   `icarus::opdet::SinglePhotonPulseFunctionTool` interface;
+   * * the resulting function describes the signal as digitized by the PMT
+   *   readout;
+   * * the function must therefore express the level of the response as
+   *   digitized ADC count above the baseline; this level is negative if the
+   *   signal polarity is negative;
+   * * the time scale of this function has `0` as the photoelectron production
+   *   time.
+   * 
+   * These requirements imply that the tool (or the underlying function) must
+   * include electronics effects like the the time it takes to form the signal,
+   * the signal shape at PMT output, the delay and distortion of the cables and
+   * the ones of the digitization electronics, but the baseline is assumed flat
+   * at 0.
    * 
    */
   class SimPMTIcarus : public art::EDProducer {
