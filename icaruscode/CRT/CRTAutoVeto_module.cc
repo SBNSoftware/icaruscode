993c5647f4f720b057dd1f5954545e2d4288ce5d
Christopher Hilgenberg <Chris.Hilgenberg@colostate.edu>
2020-06-13 15:16:20 -0500

    fix config of CRTCommonUtils and CRTBackTracker

--- icaruscode/CRT/CRTAutoVeto_module.cc
+++ icaruscode/CRT/CRTAutoVeto_module.cc
@@ -23,6 +23,8 @@
 #include "nusimdata/SimulationBase/GTruth.h"
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/GeometryCore.h"
 
 //C++ includes
 #include <vector>
@@ -50,6 +52,10 @@ namespace icarus {
 using namespace icarus::crt;
 using std::vector;
 using std::map;
+
+//namespace {
+
+//}//local namespace
 
 class icarus::crt::CRTAutoVeto : public art::EDAnalyzer {
 
@@ -81,34 +87,10 @@ class icarus::crt::CRTAutoVeto : public art::EDAnalyzer {
     double fFidZUp;
     double fFidZDown;
 
-    //histos
-    TH2F* fNuVertexXZHist;
-    TH2F* fNuVertexXYHist;
-    TH2F* fNuVertexYZHist;
-    TH2F* fNuVertexXZAVHist;
-    TH2F* fNuVertexXYAVHist; 
-    TH2F* fNuVertexYZAVHist; 
-    TH2F* fNuVertexXZFVHist; 
-    TH2F* fNuVertexXYFVHist; 
-    TH2F* fNuVertexYZFVHist; 
-                  
-    TH1F* fNuE; 
-    TH1F* fNuEAV;
-    TH1F* fNuEAVNotFV;
-    TH1F* fNuEFV;
-    TH1F* fNuEVeto;
-    TH1F* fNuEVetoAV;
-    TH1F* fNuEVetoAVNotFV;
-    TH1F* fNuEVetoFV;
-
-    TH1F* fCRTReg;
-    TH1F* fCRTRegAV;
-    TH1F* fCRTRegAVNotFV;
-    TH1F* fCRTRegFV;
-    TH1F* fNCRTReg;
-    TH1F* fNCRTRegAV;
-    TH1F* fNCRTRegAVNotFV;
-    TH1F* fNCRTRegFV;
+    //utilities
+    CRTBackTracker* bt;
+    CRTCommonUtils* crtutil;
+    geo::GeometryCore const* fGeoService;
 
     //tree
     TTree* fTree;
@@ -138,52 +120,15 @@ CRTAutoVeto::CRTAutoVeto(fhicl::ParameterSet const& p)
     fFidYTop(p.get<double>("FiducialYTop", 25.0)),
     fFidYBot(p.get<double>("FiducialYBottom",25.0)),
     fFidZUp(p.get<double>("FiducialZUpstream",30.0)),
-    fFidZDown(p.get<double>("FiducialZDownstream",50.0))
+    fFidZDown(p.get<double>("FiducialZDownstream",50.0)),
+    bt(new CRTBackTracker(p.get<fhicl::ParameterSet>("CRTBackTrack"))),
+    crtutil(new CRTCommonUtils())
 {
+    fGeoService = lar::providerFrom<geo::Geometry>();
 
     // Access ART's TFileService, which will handle creating and writing
     // histograms and n-tuples for us. 
     art::ServiceHandle<art::TFileService> tfs;
-  
-    //2D nu vertex pos
-    fNuVertexXZHist = tfs->make<TH2F>("nu_vtx_XZ", "#nu vertex: XZ; Z [cm]; X [cm]",
-                                     100, -1000, 1000, 100, -450, 450);
-    fNuVertexXYHist = tfs->make<TH2F>("nu_vtx_XY", "#nu vertex: XY; X [cm]; Y [cm]",
-                                     100, -450, 450, 100, -250, 250);
-    fNuVertexYZHist = tfs->make<TH2F>("nu_vtx_YZ", "#nu vertex: YZ; Z [cm]; Y[cm]",
-                               100, -1000, 1000, 100, -250, 250);
-    fNuVertexXZAVHist = tfs->make<TH2F>("nu_vtx_AV_XZ", "#nu vertex: XZ; Z [cm]; X [cm]",
-                               100, -1000, 1000, 100, -450, 450);
-    fNuVertexXYAVHist = tfs->make<TH2F>("nu_vtx_AV_XY", "#nu vertex: XY; X [cm]; Y [cm]",
-                               100, -450, 450, 100, -250, 250);
-    fNuVertexYZAVHist = tfs->make<TH2F>("nu_vtx_AV_YZ", "#nu vertex: YZ; Z [cm]; Y[cm]",
-                               100, -1000, 1000, 100, -250, 250);
-    fNuVertexXZFVHist = tfs->make<TH2F>("nu_vtx_FV_XZ", "#nu vertex: XZ; Z [cm]; X [cm]",
-                               100, -1000, 1000, 100, -450, 450);
-    fNuVertexXYFVHist = tfs->make<TH2F>("nu_vtx_FV_XY", "#nu vertex: XY; X [cm]; Y [cm]",
-                               100, -450, 450, 100, -250, 250);
-    fNuVertexYZFVHist = tfs->make<TH2F>("nu_vtx_FV_YZ", "#nu vertex: YZ; Z [cm]; Y[cm]",
-                               100, -1000, 1000, 100, -250, 250);
-  
-    //nu info
-    fNuE            = tfs->make<TH1F>("nuE",           "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEAV          = tfs->make<TH1F>("nuEAV",         "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEAVNotFV     = tfs->make<TH1F>("nuEAVNotFV",    "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEFV          = tfs->make<TH1F>("nuEFV",         "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEVeto        = tfs->make<TH1F>("nuEVeto",       "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEVetoAV      = tfs->make<TH1F>("nuEVetoAV",     "#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEVetoAVNotFV = tfs->make<TH1F>("nuEVetoAVNotFV","#nu energy; E_{#nu} [GeV]",100,0,10);
-    fNuEVetoFV      = tfs->make<TH1F>("nuEVetoFV",     "#nu energy; E_{#nu} [GeV]",100,0,10);
-  
-    //CRT info
-    fCRTReg         = tfs->make<TH1F>("crtReg",        "CRT Region; region ID",23,29,52);
-    fCRTRegAV       = tfs->make<TH1F>("crtRegAV",      "CRT Region; region ID",23,29,52);
-    fCRTRegAVNotFV  = tfs->make<TH1F>("crtRegAVNotFV" ,"CRT Region; region ID",23,29,52);
-    fCRTRegFV       = tfs->make<TH1F>("crtRegFV",      "CRT Region; region ID",23,29,52);
-    fNCRTReg        = tfs->make<TH1F>("nCRTReg",       "No. CRT Regions Hit; no. regions",4,0,4);
-    fNCRTRegAV      = tfs->make<TH1F>("nCRTRegAV",     "No. CRT Regions Hit; no. regions",4,0,4);
-    fNCRTRegAVNotFV = tfs->make<TH1F>("nCRTRegAVNotFV","No. CRT Regions Hit; no. regions",4,0,4);
-    fNCRTRegFV      = tfs->make<TH1F>("nCRTRegFV",     "No. CRT Regions Hit; no. regions",4,0,4);
 
     //Tree
     fTree  = tfs->make<TTree>("VetoTree", "auto-veto info");
@@ -218,7 +163,7 @@ void CRTAutoVeto::analyze(art::Event const& ev)
     
         auto const& nu = mctruth.GetNeutrino();
   
-        fNuPDG = nu.Nu().PDGCode();
+        fNuPDG = nu.Nu().PdgCode();
         fNuE   = nu.Nu().E();
   
         fNuCC = false;
@@ -229,15 +174,14 @@ void CRTAutoVeto::analyze(art::Event const& ev)
   
     }//for nus
 
-    CRTCommonUtils util(); 
     fHitRegs.clear();
     fHitXYZT.clear();
   
     //loop over CRTHits
     for(auto const& hit : crthits) {
   
-       fHitRegs.push_back(util.AuxDetRegionNameToNum(hit.tagger)); 
-       fHitXYZT.push_back({hit.x_pos,hit.y_pos,hit.z_pos,hit.ts0_ns});
+       fHitRegs.push_back(crtutil->AuxDetRegionNameToNum(hit.tagger)); 
+       fHitXYZT.push_back({hit.x_pos,hit.y_pos,hit.z_pos,(float)hit.ts0_ns});
   
     }
 
