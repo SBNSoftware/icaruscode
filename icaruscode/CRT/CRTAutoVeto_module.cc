e8bf8ab6c1f5cf37fc3a7fa07df442e913635c23
Christopher Hilgenberg <Chris.Hilgenberg@colostate.edu>
2020-06-13 19:07:07 -0500

    add geo methods to check if vertex in LAr volumes

--- icaruscode/CRT/CRTAutoVeto_module.cc
+++ icaruscode/CRT/CRTAutoVeto_module.cc
@@ -18,6 +18,7 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "art_root_io/TFileService.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
 
 //larsoft includes
 #include "nusimdata/SimulationBase/GTruth.h"
@@ -36,6 +37,7 @@
 #include <TH1F.h>
 #include <TH2F.h>
 #include <TTree.h>
+#include <TVector3.h>
 
 //local includes
 #include "icaruscode/CRT/CRTProducts/CRTHit.hh"
@@ -52,10 +54,6 @@ namespace icarus {
 using namespace icarus::crt;
 using std::vector;
 using std::map;
-
-//namespace {
-
-//}//local namespace
 
 class icarus::crt::CRTAutoVeto : public art::EDAnalyzer {
 
@@ -75,37 +73,41 @@ class icarus::crt::CRTAutoVeto : public art::EDAnalyzer {
 
  private:
 
+    bool IsAV(TVector3 const& point);
+    bool IsFV(TVector3 const& point);
+
     //config vars
     art::InputTag fGenLabel;
     art::InputTag fSimLabel;
     art::InputTag fCRTHitLabel;
+
     //define fiducial volume
-    double fFidXOut;
-    double fFidXIn;
-    double fFidYTop;
-    double fFidYBot;
-    double fFidZUp;
-    double fFidZDown;
+    double fFidXOut; //distance from outer drift LAr face, outer TPCs [cm]
+    double fFidXIn; //distance from outer drift LAr face, inner TPCs [cm]
+    double fFidYTop; //distance from top active LAr face    [cm]
+    double fFidYBot; //distance from bottom active LAr face [cm]
+    double fFidZUp;  //distance from upstream active LAr face [cm]
+    double fFidZDown; //distance from downstream active LAr face [cm]
 
     //utilities
-    CRTBackTracker* bt;
-    CRTCommonUtils* crtutil;
-    geo::GeometryCore const* fGeoService;
+    CRTBackTracker* bt;          //CRTBackTracker extracts truth info from CRT hits
+    CRTCommonUtils* crtutil;     //CRT related utilities 
+    geo::GeometryCore const* fGeoService; //access geometry
 
     //tree
-    TTree* fTree;
-    int   fEvent;
-    int   fNumNu;
-    int   fNuPDG;
-    float fNuE;
-    bool  fNuCC;
-    bool  fNuAV;
-    bool  fNuFV;
+    TTree* fTree; //pointer to analysis tree
+    int   fEvent; //art event number
+    int   fNumNu; //number of neutrinos in this event
+    int   fNuPDG; //neutrino PDG code
+    float fNuE;  //nu energy [GeV]
+    bool  fNuCC; //is the nu interaction CC?
+    bool  fNuAV; //was nu vertex in active volume?
+    bool  fNuFV; //was nu vertex in fiducial volume?
     int                   fNumHit;  //number of CRTHits
     vector<int>           fHitRegs; //region code of each hit
     vector<vector<float>> fHitXYZT; //position/time of each hit vector<{x,y,z,t}>
-
-   
+    vector<vector<int>>   fHitPDGs; //PDG code of all particles generating a CRTHit
+    vector<int>           fNHitPDGs; //number of particles contributing to CRT hit
 };//end class 
 
 
@@ -142,19 +144,25 @@ CRTAutoVeto::CRTAutoVeto(fhicl::ParameterSet const& p)
     fTree->Branch("numHit",  &fNumHit, "numHit/I");
     fTree->Branch("hitReg",  &fHitRegs);
     fTree->Branch("hitXYZT", &fHitXYZT);
-
+    fTree->Branch("hitPDG",  &fHitPDGs);
+    fTree->Branch("nHitPDG", &fNHitPDGs);
 }
 
 void CRTAutoVeto::analyze(art::Event const& ev)
 {
-    fEvent = ev.id().event();
   
-    auto const& mctruths = //vector of MCTruths
+    auto const& mctruths = //vector of MCTruths from GENIE
       *ev.getValidHandle<vector<simb::MCTruth>>(fGenLabel);
+
+    auto const& simparticles = //vector of MCParticles from G4
+      *ev.getValidHandle<vector<simb::MCParticle>>(fSimLabel);
   
     auto const& crthits = //vector of CRTHits
       *ev.getValidHandle<vector<CRTHit>>(fCRTHitLabel);
-  
+
+    map< int, const simb::MCParticle*> particleMap;
+
+    fEvent = ev.id().event();  
     fNumNu = mctruths.size();
     fNumHit = crthits.size();
   
@@ -170,23 +178,97 @@ void CRTAutoVeto::analyze(art::Event const& ev)
         if(nu.CCNC()==0)
             fNuCC = true;
   
-  
+        const TVector3 point = mctruth.GetNeutrino().Nu().Position(0).Vect();
+        fNuAV = IsAV(point);
+        fNuFV = false;
+        if(fNuAV)
+            fNuFV = IsFV(point);  
   
     }//for nus
 
+    //loop over G4 tracks
+    for(auto const& particle : simparticles){
+
+        particleMap[particle.TrackId()] = &particle;
+
+    }//G4 tracks
+
     fHitRegs.clear();
     fHitXYZT.clear();
-  
+    fHitPDGs.clear();
+    fNHitPDGs.clear();
+
     //loop over CRTHits
     for(auto const& hit : crthits) {
   
        fHitRegs.push_back(crtutil->AuxDetRegionNameToNum(hit.tagger)); 
        fHitXYZT.push_back({hit.x_pos,hit.y_pos,hit.z_pos,(float)hit.ts0_ns});
-  
+       fHitPDGs.push_back({});
+ 
+       int npdg=0;
+       for(const int id :  bt->AllTrueIds(ev,hit)){
+           if(id<0) continue;
+           if(particleMap.find(id)==particleMap.end()) continue;
+           fHitPDGs.back().push_back(particleMap[id]->PdgCode());
+           npdg++;
+       }
+       fNHitPDGs.push_back(npdg);
+
     }
 
     fTree->Fill();
 
 }//analyze
+
+bool CRTAutoVeto::IsAV(TVector3 const& point){
+
+    geo::CryostatGeo const& cryo0 = fGeoService->Cryostat(0);
+    geo::CryostatGeo const& cryo1 = fGeoService->Cryostat(1);
+    geo::TPCGeo const& tpc00 = cryo0.TPC(0);
+    geo::TPCGeo const& tpc01 = cryo0.TPC(1);
+    geo::TPCGeo const& tpc10 = cryo1.TPC(0);
+    geo::TPCGeo const& tpc11 = cryo1.TPC(1);
+
+    if(tpc00.ContainsPosition(point) ||
+       tpc01.ContainsPosition(point) ||
+       tpc10.ContainsPosition(point) ||
+       tpc11.ContainsPosition(point) )
+        return true; 
+
+    return false;
+}
+
+bool CRTAutoVeto::IsFV(TVector3 const& point) {
+
+    geo::CryostatGeo const& cryo0 = fGeoService->Cryostat(0);
+    geo::CryostatGeo const& cryo1 = fGeoService->Cryostat(1);
+    geo::TPCGeo const& tpc00 = cryo0.TPC(0);
+    geo::TPCGeo const& tpc01 = cryo0.TPC(1);
+    geo::TPCGeo const& tpc10 = cryo1.TPC(0);
+    geo::TPCGeo const& tpc11 = cryo1.TPC(1);
+
+    if(tpc00.ContainsPosition(point)
+        && tpc00.InFiducialX(point.X(),fFidXOut,fFidXIn)
+        && tpc00.InFiducialY(point.Y(),fFidYBot,fFidYTop)
+        && tpc00.InFiducialZ(point.Z(),fFidZUp,fFidZDown) )
+        return true;
+    if(tpc01.ContainsPosition(point)
+        && tpc01.InFiducialX(point.X(),fFidXOut,fFidXIn)
+        && tpc01.InFiducialY(point.Y(),fFidYBot,fFidYTop)
+        && tpc01.InFiducialZ(point.Z(),fFidZUp,fFidZDown) )
+        return true;
+    if(tpc10.ContainsPosition(point)
+        && tpc10.InFiducialX(point.X(),fFidXOut,fFidXIn)
+        && tpc10.InFiducialY(point.Y(),fFidYBot,fFidYTop)
+        && tpc10.InFiducialZ(point.Z(),fFidZUp,fFidZDown) )
+        return true;
+    if(tpc11.ContainsPosition(point)
+        && tpc11.InFiducialX(point.X(),fFidXIn,fFidXOut)
+        && tpc11.InFiducialY(point.Y(),fFidYBot,fFidYTop)
+        && tpc11.InFiducialZ(point.Z(),fFidZUp,fFidZDown) )
+        return true;
+
+    return false;
+}
 
 DEFINE_ART_MODULE(CRTAutoVeto)
