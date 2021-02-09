14ffcc340b505355cccdbe6e17e30dbb111ef305
aaduszki <55505042+aaduszki@users.noreply.github.com>
2021-02-09 13:12:02 -0600

    Update BernCRTTranslator.hh
    Added comments

--- icaruscode/CRT/CRTDecoder/BernCRTTranslator.hh
+++ icaruscode/CRT/CRTDecoder/BernCRTTranslator.hh
@@ -26,23 +26,23 @@ namespace icarus::crt {
 class icarus::crt::BernCRTTranslator {
 
 public:
-  uint8_t   mac5                    = 0;
-  uint64_t  run_start_time          = 0;
-  uint64_t  this_poll_start         = 0;
-  uint64_t  this_poll_end           = 0;
-  uint64_t  last_poll_start         = 0;
-  uint64_t  last_poll_end           = 0;
-  int32_t   system_clock_deviation  = 0; //system clock deviation w.r.t. steady clock, synchronised at the beginning of the run
-  uint32_t  hits_in_poll            = 0; //includes lost ones
+  uint8_t   mac5                    = 0; //last 8 bits of FEB MAC5 address
+  uint64_t  run_start_time          = 0; //absolute timestamp [ns]
+  uint64_t  this_poll_start         = 0; //absolute timestamp [ns]
+  uint64_t  this_poll_end           = 0; //absolute timestamp [ns]
+  uint64_t  last_poll_start         = 0; //absolute timestamp [ns]
+  uint64_t  last_poll_end           = 0; //absolute timestamp [ns]
+  int32_t   system_clock_deviation  = 0; //system clock deviation [ns] w.r.t. steady clock, synchronised at the beginning of the run
+  uint32_t  hits_in_poll            = 0; //includes lost hits
   uint16_t  hits_in_fragment        = 0; //size of the fragment hit array
 
-  uint8_t  flags                    = 0;
-  uint16_t lostcpu                  = 0;
-  uint16_t lostfpga                 = 0;
-  uint32_t ts0                      = 0;
-  uint32_t ts1                      = 0;
-  uint16_t adc[32]                  = {0};
-  uint32_t coinc                    = 0;
+  uint8_t  flags                    = 0; //see corresponding methods
+  uint16_t lostcpu                  = 0; //hits lost before the trigger hit due to too long polling time
+  uint16_t lostfpga                 = 0; //hits lost after the trigger hit due to dead time
+  uint32_t ts0                      = 0; //number of [ns] since last T0 signal
+  uint32_t ts1                      = 0; //number of [ns] since last T1 signal
+  uint16_t adc[32]                  = {0}; //ADC readout for each of 32 FEB channels
+  uint32_t coinc                    = 0; //used by TOP CRT
 
   //Data added by fragment generator
   uint64_t  feb_hit_number          = 0; //hit counter for individual FEB, including hits lost in FEB or fragment generator
