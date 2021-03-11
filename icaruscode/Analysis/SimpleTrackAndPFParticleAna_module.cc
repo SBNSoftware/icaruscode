329c0914d15556c8639345413a290200a0b9db87
Bruce Howard <howard@fnal.gov>
2021-03-10 19:23:52 -0600

    Several changes to SimpleTrackAndPFParticleAna. I believe major changes are as follows (a) bug-fix for upstream and downstream in FV check, (b) added fcl switchable option to print out potential anode-cathode crossing tracks -- right now a simple check on x distance being close to 1.5m -- as INFO messages, (c) added new plots and variables and such, (d) hopefully more useful WARNING messages.

--- icaruscode/Analysis/SimpleTrackAndPFParticleAna_module.cc
+++ icaruscode/Analysis/SimpleTrackAndPFParticleAna_module.cc
@@ -31,7 +31,13 @@
 #include "lardataobj/RecoBase/Track.h"
 #include "larcore/Geometry/Geometry.h"
 
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "TH1D.h"
+#include "TH2D.h"
 
 class SimpleTrackAndPFParticleAna;
 
@@ -58,6 +64,7 @@ private:
   // fcl params
   art::InputTag fPFParticleModuleLabel; // Label for PFParticles
   art::InputTag fTrackModuleLabel;      // Label for tracks
+  bool          fPrintAnodeToCathode;   // Print out anode-cathode crossing candidates
   bool          fUseFV;                 // Use a fiducial volume defined by the Tolerances
   double        fTolUsDs;               // Upstream/Downstream tolerance for vertex
   double        fTolAnode;              // Anode tolerance for vertex
@@ -69,11 +76,21 @@ private:
   TH1D *vtxDetX;
   TH1D *vtxDetY;
   TH1D *vtxDetZ;
+  TH2D *vtxDetZX;
+  TH2D *vtxDetXY;
+
   TH1D *trkMaxLength;
-  TH1D *trkMaxLengthX;
+  TH1D *trkMaxLengthX; // projected length using direction and length
   TH1D *trkLengthAll;
-  TH1D *trkLengthAllX;
-  TH1D *trkLengthAllY;
+  TH1D *trkLengthAllX; // projected length using direction and length
+  TH1D *trkLengthAllY; // projected length using direction and length
+
+  TH1D *trkStartX;
+  TH1D *trkEndX;
+  TH2D *trkStartEndX;
+  TH1D *trkDistX;      // actual End_x - Start_x
+  TH1D *trkDistY;      // actual End_y - Start_y
+  TH1D *trkDistZ;      // actual End_z - Start_z
 };
 
 
@@ -81,6 +98,7 @@ SimpleTrackAndPFParticleAna::SimpleTrackAndPFParticleAna(fhicl::ParameterSet con
   : EDAnalyzer{p},
   fPFParticleModuleLabel( p.get< art::InputTag >("PFParticleModuleLabel") ),
   fTrackModuleLabel( p.get< art::InputTag >("TrackModuleLabel") ),
+  fPrintAnodeToCathode( p.get< bool >("PrintAnodeToCathode") ),
   fUseFV( p.get< bool >("UseFV") ),
   fTolUsDs( p.get< double >("TolUsDs") ),
   fTolAnode( p.get< double >("TolAnode") ),
@@ -90,16 +108,26 @@ SimpleTrackAndPFParticleAna::SimpleTrackAndPFParticleAna(fhicl::ParameterSet con
   // Call appropriate consumes<>() for any products to be retrieved by this module.
 
   art::ServiceHandle<art::TFileService> tfs;
-  tfs->mkdir("SimpleScanFilter");
-  pfpCounter = tfs->make<TH1D>("SimpleScan-pfpCounter",";PFParticles;Events",300,0,300);
-  vtxDetX = tfs->make<TH1D>("SimpleScan_vtxDetX",";Detector X [cm];Events",100,-500,500);
-  vtxDetY = tfs->make<TH1D>("SimpleScan_vtxDetY",";Detector Y [cm];Events",40,-200,200);
-  vtxDetZ = tfs->make<TH1D>("SimpleScan_vtxDetZ",";Detector Z [cm];Events",100,-1000,1000);
-  trkMaxLength = tfs->make<TH1D>("SimpleScan_trkMaxLength",";Longest track length, vertex contained [cm];Events",503,-6,1000);
-  trkMaxLengthX = tfs->make<TH1D>("SimpleScan_trkMaxLengthX",";Longest track X length, vertex contained [cm];Events",503,-6,1000);
-  trkLengthAll = tfs->make<TH1D>("SimpleScan_trkLengthAll",";All track lengths, vertex contained [cm];Events",503,-6,1000);
-  trkLengthAllX = tfs->make<TH1D>("SimpleScan_trkLengthAllX",";All track X lengths, vertex contained [cm];Events",503,-6,1000);
-  trkLengthAllY = tfs->make<TH1D>("SimpleScan_trkLengthAllY",";All track Y lengths, vertex contained [cm];Events",503,-6,1000);
+  tfs->mkdir("SimpleTrackAndPFParticleAna");
+  pfpCounter = tfs->make<TH1D>("SimpleAnalysis_pfpCounter",";PFParticles;Events",1000,0,1000); //300,0,300);
+  vtxDetX = tfs->make<TH1D>("SimpleAnalysis_vtxDetX",";Detector X [cm];Events",100,-500,500);
+  vtxDetY = tfs->make<TH1D>("SimpleAnalysis_vtxDetY",";Detector Y [cm];Events",40,-200,200);
+  vtxDetZ = tfs->make<TH1D>("SimpleAnalysis_vtxDetZ",";Detector Z [cm];Events",100,-1000,1000);
+  vtxDetZX = tfs->make<TH2D>("SimpleAnalysis_vtxDetZX",";Detector Z [cm];Detector X [cm]",100,-1000,1000,100,-500,500);
+  vtxDetXY = tfs->make<TH2D>("SimpleAnalysis_vtxDetXY",";Detector X [cm];Detector Y [cm]",100,-500,500,40,-200,200);
+
+  trkMaxLength = tfs->make<TH1D>("SimpleAnalysis_trkMaxLength",";Longest track length [cm];Events",503,-6,1000);
+  trkMaxLengthX = tfs->make<TH1D>("SimpleAnalysis_trkMaxLengthX",";Longest track X (projected) length [cm];Events",503,-6,1000);
+  trkLengthAll = tfs->make<TH1D>("SimpleAnalysis_trkLengthAll",";All considered track lengths [cm];Events",503,-6,1000);
+  trkLengthAllX = tfs->make<TH1D>("SimpleAnalysis_trkLengthAllX",";All considered track X (projected) lengths [cm];Events",503,-6,1000);
+  trkLengthAllY = tfs->make<TH1D>("SimpleAnalysis_trkLengthAllY",";All considered track Y (projected) lengths [cm];Events",503,-6,1000);
+
+  trkStartX = tfs->make<TH1D>("SimpleAnalysis_trkStartX",";Track Start X [cm];Events",100,-500,500);
+  trkEndX = tfs->make<TH1D>("SimpleAnalysis_trkEndX",";Track End X [cm];Events",100,-500,500);
+  trkStartEndX = tfs->make<TH2D>("SimpleAnalysis_trkStartEndX",";Track Start X [cm];Track End X",100,-500,500,100,-500,500);
+  trkDistX = tfs->make<TH1D>("SimpleAnalysis_trkDistX",";Track Dist X [cm];Events",252,-8,1000);
+  trkDistY = tfs->make<TH1D>("SimpleAnalysis_trkDistY",";Track Dist Y [cm];Events",252,-8,1000);
+  trkDistZ = tfs->make<TH1D>("SimpleAnalysis_trkDistZ",";Track Dist Z [cm];Events",502,-8,2000);
 }
 
 void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
@@ -115,14 +143,16 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
     art::fill_ptr_vector(pfparticles,pfparticleHandle);
   }
   else{
-    mf::LogWarning("SimpleScanFilter") << "Event failed to find recob::PFParticle. Are you sure you gave the right label and are running this at the right time?";
+    mf::LogWarning("SimpleTrackAndPFParticleAna") << "Event failed to find recob::PFParticle. Are you sure you gave the right label and are running this at the right time?";
     return;
   }
-  pfpCounter->Fill( pfparticles.size() ); 
+  pfpCounter->Fill( pfparticles.size() );
+
+  std::cout << "pfparticles.size() = " << pfparticles.size() << std::endl;
 
   if( pfparticles.size()==0 ){
     // If no PFParticles then skip, but give a message
-    mf::LogWarning("SimpleScanFilter") << "No PFParticles in the considered event.";
+    mf::LogWarning("SimpleTrackAndPFParticleAna") << "No PFParticles in the considered event.";
     return;
   }
 
@@ -132,14 +162,14 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
   art::FindManyP<recob::Vertex> fmvtx(pfparticleHandle, e, fPFParticleModuleLabel);
   if( !fmvtx.isValid() ){
     // Check against validity of fmvtx (using isValid, as used in an analyzer module from SBND)
-    mf::LogError("SimpleScanFilter") << "Error in validity of fmvtx. Retruning false.";
+    mf::LogError("SimpleTrackAndPFParticleAna") << "Error in validity of fmvtx. Retruning false.";
     return;
   }
   if( fmvtx.size()==0 ){
     // Check if any vertices, if not, skip event
     if( pfparticles.size()==0 )
-      mf::LogWarning("SimpleScanFilter") << "Event had no vertices, but no PFParticles. Events with no PFParticles were meant to be filtered earlier... Returning false.";
-    else mf::LogError("SimpleScanFilter") << "Event had no vertices, but had PFParticles. Returning false.";
+      mf::LogWarning("SimpleTrackAndPFParticleAna") << "Event had no vertices, but no PFParticles. Events with no PFParticles were meant to be filtered earlier... Returning false.";
+    else mf::LogError("SimpleTrackAndPFParticleAna") << "Event had no vertices, but had PFParticles. Returning false.";
     return;
   }
 
@@ -149,7 +179,7 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
   art::FindManyP<recob::Track> fmtrk(pfparticleHandle, e, fTrackModuleLabel);
   if( !fmtrk.isValid() ){
     // Check against validity of fmtrk (using isValid, as used in an analyzer module from SBND)
-    mf::LogError("SimpleScanFilter") << "Error in validity of fmtrk. Returning false.";
+    mf::LogError("SimpleTrackAndPFParticleAna") << "Error in validity of fmtrk. Returning false.";
     return;
   }
 
@@ -172,7 +202,7 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
     // access the associated vertex
     std::vector< art::Ptr<recob::Vertex> > pfpVtx = fmvtx.at(iPfp.key());
     if( pfpVtx.size()!=1 ){
-      mf::LogWarning("SimpleScanFilter") << "Zero/multiple vertices found for given PFParticle! Skipping this PFParticle but take note...";
+      mf::LogWarning("SimpleTrackAndPFParticleAna") << "Zero/multiple vertices (" << pfpVtx.size() << ") found for given PFParticle! Skipping this PFParticle but take note...";
       continue;
     }
     double vtxX = pfpVtx.at(0)->position().X();
@@ -182,13 +212,15 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
     vtxDetX->Fill( vtxX );
     vtxDetY->Fill( vtxY );
     vtxDetZ->Fill( vtxZ );
+    vtxDetZX->Fill( vtxZ, vtxX );
+    vtxDetXY->Fill( vtxX, vtxY );
 
     // IF CHECKING FIDUCIAL VOLUME
     // get positions to check against
     if( fUseFV ){
       auto vtxTPC = geom->PositionToTPCptr( pfpVtx.at(0)->position() ); // tpc containing the vertex
       if( vtxTPC == nullptr ){
-	mf::LogDebug("SimpleScanFilter") << "geom->PositionToTPCptr not valid. Vertex: (" << vtxX << "," << vtxY << "," << vtxZ <<  "). Skipping this PFParticle";
+	mf::LogDebug("SimpleTrackAndPFParticleAna") << "geom->PositionToTPCptr not valid. Vertex: (" << vtxX << "," << vtxY << "," << vtxZ <<  "). Skipping this PFParticle";
 	continue;
       }
       double posAnode = (vtxTPC->DetectDriftDirection()==1 ? vtxTPC->ActiveBoundingBox().MaxX() : vtxTPC->ActiveBoundingBox().MinX()); // if drift +x, then anode = x max
@@ -199,8 +231,8 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
       double posDsZ = vtxTPC->ActiveBoundingBox().MaxZ(); // downstream z
 
       // check that the vertex is within allowed tolerances
-      if( !( posUsZ>0.5 ? (posUsZ-vtxZ)>fTolUsDs : true ) || 
-	  !( posDsZ<-0.5 ? (vtxZ-posDsZ)>fTolUsDs : true ) || 
+      if( !( posDsZ>0.5 ? (posDsZ-vtxZ)>fTolUsDs : true ) ||
+	  !( posUsZ<-0.5 ? (vtxZ-posUsZ)>fTolUsDs : true ) ||
 	  !( vtxTPC->DetectDriftDirection()==1 ? ( (posAnode-vtxX)>fTolAnode && (vtxX-posCathode)>fTolCathode ) : 
 	     ( (posCathode-vtxX)>fTolCathode && (vtxX-posAnode)>fTolAnode ) ) ||
 	  !( (posTopY-vtxY)>fTolTopBot ) ||
@@ -213,21 +245,40 @@ void SimpleTrackAndPFParticleAna::analyze(art::Event const& e)
     // access the associated track
     std::vector< art::Ptr<recob::Track> > pfpTrk = fmtrk.at(iPfp.key());
     if( pfpTrk.size()!=1 ){
-      mf::LogWarning("SimpleScanFilter") << "Zero/multiple tracks found for given PFParticle (after checking it was a track). Skipping this PFParticle but take note...";
+      mf::LogWarning("SimpleTrackAndPFParticleAna") << "Zero/multiple (" << pfpTrk.size() << ") tracks found for PFParticle (after checking it was a track). Skipping this PFParticle but take note...";
       continue;
     }
     double trkLength = pfpTrk.at(0)->Length();
+    double trkX0 = pfpTrk.at(0)->Start().X();
+    double trkXf = pfpTrk.at(0)->End().X();
+    double trkY0 = pfpTrk.at(0)->Start().Y();
+    double trkYf = pfpTrk.at(0)->End().Y();
+    double trkZ0 = pfpTrk.at(0)->Start().Z();
+    double trkZf = pfpTrk.at(0)->End().Z();
 
-    std::cout << "length = " << trkLength 
-	      << " ... Dir X = " << pfpTrk.at(0)->StartDirection().Unit().X() 
-	      << " ... Dir Y = " << pfpTrk.at(0)->StartDirection().Unit().Y() << std::endl;
+    //    std::cout << "length = " << trkLength
+    //	      << " ... Dir X = " << pfpTrk.at(0)->StartDirection().Unit().X()
+    //	      << " ... Dir Y = " << pfpTrk.at(0)->StartDirection().Unit().Y() << std::endl;
 
     trkLengthAll->Fill( trkLength );
     trkLengthAllX->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X()) );
     trkLengthAllY->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().Y()) );
+
+    trkStartX->Fill( trkX0 );
+    trkEndX->Fill( trkXf );
+    trkStartEndX->Fill( trkX0, trkXf );
+    trkDistX->Fill( fabs(trkXf-trkX0) );
+    trkDistY->Fill( fabs(trkYf-trkY0) );
+    trkDistZ->Fill( fabs(trkZf-trkZ0) );
+
     if( trkLength > maxTrkLength ){
       maxTrkLength = trkLength;
       maxTrkLengthX = trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X());
+    }
+
+    if( fPrintAnodeToCathode && fabs(trkXf-trkX0)>=142.5 ){
+      mf::LogInfo("SimpleTrackAndPFParticleAna") << "Potential anode-to-cathode crossing track with length (" << fabs(trkXf-trkX0)
+						 << ") and start point (" << trkX0 << ", " << trkY0 << ", " << trkZ0 << ")";
     }
   }
 
