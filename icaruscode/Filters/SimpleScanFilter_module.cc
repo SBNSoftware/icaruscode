7adeeb4845a0dbb17dd8b3070eb510d7d610d5b6
Bruce Howard <howard@fnal.gov>
2021-03-23 00:20:20 -0500

    Updates to current version of SimpleScanFilter being worked on.

--- icaruscode/Filters/SimpleScanFilter_module.cc
+++ icaruscode/Filters/SimpleScanFilter_module.cc
@@ -30,11 +30,17 @@
 #include "lardataobj/RecoBase/Vertex.h"
 #include "lardataobj/RecoBase/Track.h"
 #include "larcore/Geometry/Geometry.h"
+#include "larpandora/LArPandoraInterface/LArPandoraHelper.h"
 
 // ROOT
 #include "TH1D.h"
+#include "TH2D.h"
 
 #include <memory>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
 
 class SimpleScanFilter;
 
@@ -62,13 +68,16 @@ private:
   art::InputTag fPFParticleModuleLabel; // Label for PFParticles
   art::InputTag fTrackModuleLabel;      // Label for tracks
   bool          fCompleteLoop;          // Sets whether to end the loop early or make debug plots
+  bool          fContainTracks;         // Sets whether to check for track containment
   double        fTolUsDs;               // Upstream/Downstream tolerance for vertex
   double        fTolAnode;              // Anode tolerance for vertex
   double        fTolTopBot;             // Top/Bottom tolerance for vertex
   double        fTolCathode;            // Cathode tolerance for vertex
+  double        fTolContainTrack;       // Tolerance for track containment on all sides
 
   // histograms
   TH1D *pfpCounter;
+  TH1D *pfpNuCounter;
   TH1D *vtxDetX;
   TH1D *vtxDetY;
   TH1D *vtxDetZ;
@@ -77,6 +86,28 @@ private:
   TH1D *trkLengthAll;
   TH1D *trkLengthAllX;
   TH1D *trkLengthAllY;
+  TH1D *trkMaxDirZ;
+  TH2D *trkMaxLengthVsTrkMaxDirZ;
+
+  // Active volume map - will be filled out in constructor
+  std::map< geo::TPCID, short int > mapDriftDir;
+  std::map< geo::TPCID, double >    mapPosAnode;
+  std::map< geo::TPCID, double >    mapPosCathode;
+  std::map< geo::TPCID, double >    mapPosTopY;
+  std::map< geo::TPCID, double >    mapPosBotY;
+  std::map< geo::TPCID, double >    mapPosUsZ;
+  std::map< geo::TPCID, double >    mapPosDsZ;
+
+  // TPC min and max combined for containment volume
+  double tpcContXMin;
+  double tpcContXMax;
+  double tpcContYMin;
+  double tpcContYMax;
+  double tpcContZMin;
+  double tpcContZMax;
+
+  // Services
+  geo::GeometryCore const* fGeom;
 };
 
 
@@ -85,17 +116,20 @@ SimpleScanFilter::SimpleScanFilter(fhicl::ParameterSet const& p)
     fPFParticleModuleLabel( p.get< art::InputTag >("PFParticleModuleLabel") ),
     fTrackModuleLabel( p.get< art::InputTag >("TrackModuleLabel") ),
     fCompleteLoop( p.get< bool >("CompleteLoop") ),
+    fContainTracks( p.get< bool >("ContainTracks") ),
     fTolUsDs( p.get< double >("TolUsDs") ),
     fTolAnode( p.get< double >("TolAnode") ),
     fTolTopBot( p.get< double >("TolTopBot") ),
-    fTolCathode( p.get< double >("TolCathode") )
+    fTolCathode( p.get< double >("TolCathode") ),
+    fTolContainTrack( p.get< double >("TolContainTrack") )
 {
   // Call appropriate produces<>() functions here.
   // Call appropriate consumes<>() for any products to be retrieved by this module.
 
   art::ServiceHandle<art::TFileService> tfs;
   tfs->mkdir("SimpleScanFilter");
-  pfpCounter = tfs->make<TH1D>("SimpleScan-pfpCounter",";PFParticles;Events",300,0,300);
+  pfpCounter = tfs->make<TH1D>("SimpleScan_pfpCounter",";PFParticles;Events",1000,0,1000);
+  pfpNuCounter = tfs->make<TH1D>("SimpleScan_pfpNuCounter",";PFP Neutrinos;Events",1000,0,1000);
   vtxDetX = tfs->make<TH1D>("SimpleScan_vtxDetX",";Detector X [cm];Events",100,-500,500);
   vtxDetY = tfs->make<TH1D>("SimpleScan_vtxDetY",";Detector Y [cm];Events",40,-200,200);
   vtxDetZ = tfs->make<TH1D>("SimpleScan_vtxDetZ",";Detector Z [cm];Events",100,-1000,1000);
@@ -104,6 +138,59 @@ SimpleScanFilter::SimpleScanFilter(fhicl::ParameterSet const& p)
   trkLengthAll = tfs->make<TH1D>("SimpleScan_trkLengthAll",";All track lengths, vertex contained [cm];Events",503,-6,1000);
   trkLengthAllX = tfs->make<TH1D>("SimpleScan_trkLengthAllX",";All track X lengths, vertex contained [cm];Events",503,-6,1000);
   trkLengthAllY = tfs->make<TH1D>("SimpleScan_trkLengthAllY",";All track Y lengths, vertex contained [cm];Events",503,-6,1000);
+  trkMaxDirZ = tfs->make<TH1D>("SimpleScan_trkMaxDirZ",";Z component of direction - longest accepted track;Events",43,-1.15,1);
+  trkMaxLengthVsTrkMaxDirZ = tfs->make<TH2D>("SimpleScan_trkMaxLengthVsTrkMaxDirZ",";Z component of direction - longest accepted track;Track Length",9,-1.25,1,9,-50,400);
+
+  // Set up geometry
+  //  art::ServiceHandle<geo::Geometry> geom;
+  fGeom = lar::providerFrom<geo::Geometry>();
+
+  for( geo::TPCID const& thisTPC : fGeom->IterateTPCIDs() ){
+    auto thisTPCPtr = fGeom->TPCPtr(thisTPC);
+    short int thisDriftDir = thisTPCPtr->DetectDriftDirection();
+
+    mapDriftDir[ thisTPC ]   = thisDriftDir;
+    mapPosAnode[ thisTPC ]   = (thisDriftDir==1 ? thisTPCPtr->ActiveBoundingBox().MaxX() : thisTPCPtr->ActiveBoundingBox().MinX()); // if drift +x, then anode = x max
+    mapPosCathode[ thisTPC ] = (thisDriftDir==-1 ? thisTPCPtr->ActiveBoundingBox().MaxX() : thisTPCPtr->ActiveBoundingBox().MinX()); // opposite of anode
+    mapPosTopY[ thisTPC ]    = thisTPCPtr->ActiveBoundingBox().MaxY(); // top y of AV
+    mapPosBotY[ thisTPC ]    = thisTPCPtr->ActiveBoundingBox().MinY(); // bottom y of AV
+    mapPosUsZ[ thisTPC ]     = thisTPCPtr->ActiveBoundingBox().MinZ(); // upstream z
+    mapPosDsZ[ thisTPC ]     = thisTPCPtr->ActiveBoundingBox().MaxZ(); // downstream z
+  }
+
+  // this time use the map to get the min and max x,y,z of the TPCs
+  // TODO: probably a better way to do this...
+  int counterTPC = 0;
+  for( geo::TPCID const& thisTPC : fGeom->IterateTPCIDs() ){
+    if(counterTPC==0){
+      tpcContXMin = std::min( mapPosAnode[thisTPC], mapPosCathode[thisTPC] );
+      tpcContXMax = std::max( mapPosAnode[thisTPC], mapPosCathode[thisTPC] );
+      tpcContZMax = mapPosDsZ[thisTPC];
+      tpcContZMin = mapPosUsZ[thisTPC];
+      tpcContYMax = mapPosTopY[thisTPC];
+      tpcContYMin = mapPosBotY[thisTPC];
+    }
+    else{
+      if( mapPosAnode[thisTPC] < tpcContXMin )        tpcContXMin = mapPosAnode[thisTPC];
+      else if( mapPosAnode[thisTPC] > tpcContXMax )   tpcContXMax = mapPosAnode[thisTPC];
+
+      if( mapPosCathode[thisTPC] < tpcContXMin )      tpcContXMin = mapPosCathode[thisTPC];
+      else if( mapPosCathode[thisTPC] > tpcContXMax ) tpcContXMax = mapPosCathode[thisTPC];
+
+      if( mapPosTopY[thisTPC] < tpcContYMin )         tpcContYMin = mapPosTopY[thisTPC];
+      else if( mapPosTopY[thisTPC] > tpcContYMax )    tpcContYMax = mapPosTopY[thisTPC];
+
+      if( mapPosBotY[thisTPC] < tpcContYMin )         tpcContYMin = mapPosBotY[thisTPC];
+      else if( mapPosBotY[thisTPC] > tpcContYMax )    tpcContYMax = mapPosBotY[thisTPC];
+
+      if( mapPosDsZ[thisTPC] < tpcContZMin )          tpcContZMin = mapPosDsZ[thisTPC];
+      else if( mapPosDsZ[thisTPC] > tpcContZMax )     tpcContZMax = mapPosDsZ[thisTPC];
+
+      if( mapPosUsZ[thisTPC] < tpcContZMin )          tpcContZMin = mapPosUsZ[thisTPC];
+      else if( mapPosUsZ[thisTPC] > tpcContZMax )     tpcContZMax = mapPosUsZ[thisTPC];
+    }
+    counterTPC+=1;
+  }
 }
 
 bool SimpleScanFilter::filter(art::Event& e)
@@ -112,7 +199,8 @@ bool SimpleScanFilter::filter(art::Event& e)
   // Get the PFParticles
   ///////////////////////////////
   art::Handle< std::vector<recob::PFParticle> > pfparticleHandle;
-  std::vector< art::Ptr<recob::PFParticle> > pfparticles;
+  //std::vector< art::Ptr<recob::PFParticle> > pfparticles;
+  lar_pandora::PFParticleVector pfparticles;
   if( e.getByLabel(fPFParticleModuleLabel,pfparticleHandle) ){
     art::fill_ptr_vector(pfparticles,pfparticleHandle);
   }
@@ -127,6 +215,10 @@ bool SimpleScanFilter::filter(art::Event& e)
     mf::LogWarning("SimpleScanFilter") << "No PFParticles in the considered event.";
     return false;
   }
+
+  // PFParticle Map
+  lar_pandora::PFParticleMap pfpMap;
+  lar_pandora::LArPandoraHelper::BuildPFParticleMap(pfparticles,pfpMap);
 
   ///////////////////////////////
   // Get the vertex association to the PFPs
@@ -157,31 +249,40 @@ bool SimpleScanFilter::filter(art::Event& e)
 
   ///////////////////////////////
   // Get geometry
-  /////////////////////////////// 
-  art::ServiceHandle<geo::Geometry> geom;
+  ///////////////////////////////
+  fGeom = lar::providerFrom<geo::Geometry>();
 
   ///////////////////////////////
   // Loop through the PFPs and do the checks
   ///////////////////////////////
   // Note: If efficiency matters, then probably you want to return true as soon as you find a
   // a track of >50cm meeting the vertex criteria. But, to make some debug plots, then set
-  //   CompleteLoop: true 
-  // in the fcl settings and it will find the maximum track length for tracks with vertices 
+  //   CompleteLoop: true
+  // in the fcl settings and it will find the maximum track length for tracks with vertices
   // meeting the criteria, and will fill additional stats in the vertex plots.
 
-  //  std::cout << "Size of pfparticles " << fPFParticleModuleLabel << ": " << pfparticles.size() << std::endl;
-  float maxTrkLength = -5.;
-  float maxTrkLengthX = -5.;
+  double maxTrkLengthVal = -5.;
+  double maxTrkLengthXVal = -5.;
+  double maxTrkDirZVal = -1.14;
+  int pfpNus = 0;
+
   for( auto const& iPfp : pfparticles ){
-    ////    std::cout << iPfp->PdgCode() << std::endl;
-    if(abs(iPfp->PdgCode())!=13 ) continue;
+    // FOR CHECKING JUST TRACKS
+    //    if(abs(iPfp->PdgCode())!=13 ) continue;
+
+    // CHECKING PFP NUs
+    if( abs(iPfp->PdgCode()!=12 && iPfp->PdgCode()!=14) ) continue;
+    pfpNus+=1;
+
+    // debug print out useful to look at pfparticle hierarchies
+    mf::LogDebug("SimpleScanFilter") << "PFParticle Nu with ID: " << iPfp.key();
 
     // CHECK VERTEX
     // -------------------
     // access the associated vertex
     std::vector< art::Ptr<recob::Vertex> > pfpVtx = fmvtx.at(iPfp.key());
     if( pfpVtx.size()!=1 ){
-      mf::LogWarning("SimpleScanFilter") << "Zero/multiple vertices found for given PFParticle! Skipping this PFParticle but take note...";
+      mf::LogWarning("SimpleScanFilter") << "Zero/multiple vertices (" << pfpVtx.size() << ") found for given PFParticle! Skipping this PFParticle but take note...";
       continue;
     }
     double vtxX = pfpVtx.at(0)->position().X();
@@ -194,66 +295,103 @@ bool SimpleScanFilter::filter(art::Event& e)
       vtxDetZ->Fill( vtxZ );
     }
 
-    // get positions to check against
-    auto vtxTPC = geom->PositionToTPCptr( pfpVtx.at(0)->position() ); // tpc containing the vertex
-    if( vtxTPC == nullptr ){
-      mf::LogDebug("SimpleScanFilter") << "geom->PositionToTPCptr not valid. Vertex: (" << vtxX << "," << vtxY << "," << vtxZ <<  "). Skipping this PFParticle";
-      continue;
+    // debug print out useful to look at pfparticle hierarchies
+    mf::LogDebug("SimpleScanFilter") << "PFParticle Nu Vtx: " << vtxX << ", " << vtxY << ", " << vtxZ;
+
+    // check against the positions in the map and the tolerances
+    auto thisVtxTPC = fGeom->PositionToTPCID( pfpVtx.at(0)->position() );
+    if( !thisVtxTPC.isValid ){
+      // TODO: is this doing anything...
+      mf::LogWarning("SimpleScanFilter") << "fGeom->PositionToTPCID not valid. Vertex: (" << vtxX << "," << vtxY << "," << vtxZ <<  "). Skipping this PFParticle";
+      continue; 
     }
-    double posAnode = (vtxTPC->DetectDriftDirection()==1 ? vtxTPC->ActiveBoundingBox().MaxX() : vtxTPC->ActiveBoundingBox().MinX()); // if drift +x, then anode = x max
-    double posCathode = (vtxTPC->DetectDriftDirection()==-1 ? vtxTPC->ActiveBoundingBox().MaxX() : vtxTPC->ActiveBoundingBox().MinX()); // opposite of anode
-    double posTopY = vtxTPC->ActiveBoundingBox().MaxY(); // top y of AV
-    double posBotY = vtxTPC->ActiveBoundingBox().MinY(); // bottom y of AV
-    double posUsZ = vtxTPC->ActiveBoundingBox().MinZ(); // upstream z
-    double posDsZ = vtxTPC->ActiveBoundingBox().MaxZ(); // downstream z
 
-    ////    std::cout << "TPC" << vtxTPC->TPCInfo() << std::endl;
-    ////    std::cout << "Anode: " << posAnode << ", Cathode: " << posCathode << std::endl;
-    ////    std::cout << "Top Y: " << posTopY << ", Bot Y: " << posBotY << std::endl;
-    ////    std::cout << "Upstream Z: " << posUsZ << ", Downstream Z: " << posDsZ << std::endl;
-    ////    std::cout << "" << std::endl;
-
-    // check that the vertex is within allowed tolerances
-    if( !( posUsZ>0.5 ? (posUsZ-vtxZ)>fTolUsDs : true ) || 
-	!( posDsZ<-0.5 ? (vtxZ-posDsZ)>fTolUsDs : true ) || 
-	!( vtxTPC->DetectDriftDirection()==1 ? ( (posAnode-vtxX)>fTolAnode && (vtxX-posCathode)>fTolCathode ) : 
-	                                      ( (posCathode-vtxX)>fTolCathode && (vtxX-posAnode)>fTolAnode ) ) ||
-	!( (posTopY-vtxY)>fTolTopBot ) ||
-	!( (vtxY-posBotY)>fTolTopBot ) )
+    // Us/Ds fixed here I think... need to propagate to the analyzer module too...
+    if( !( mapPosDsZ[thisVtxTPC]>0.5 ? (mapPosDsZ[thisVtxTPC]-vtxZ)>fTolUsDs : true ) ||
+        !( mapPosUsZ[thisVtxTPC]<-0.5 ? (vtxZ-mapPosUsZ[thisVtxTPC])>fTolUsDs : true ) ||
+        !( mapDriftDir[thisVtxTPC]==1 ? ( (mapPosAnode[thisVtxTPC]-vtxX)>fTolAnode && (vtxX-mapPosCathode[thisVtxTPC])>fTolCathode ) :
+	   ( (mapPosCathode[thisVtxTPC]-vtxX)>fTolCathode && (vtxX-mapPosAnode[thisVtxTPC])>fTolAnode ) ) ||
+        !( (mapPosTopY[thisVtxTPC]-vtxY)>fTolTopBot ) ||
+        !( (vtxY-mapPosBotY[thisVtxTPC])>fTolTopBot ) )
       continue;
 
-    // CHECK TRACK
-    // -------------------
-    // access the associated track, if PFParticle's PDG Code == +/- 13
-    std::vector< art::Ptr<recob::Track> > pfpTrk = fmtrk.at(iPfp.key());
-    ////    std::cout << pfpTrk.size() << std::endl;
-    if( pfpTrk.size()!=1 ){
-      mf::LogWarning("SimpleScanFilter") << "Zero/multiple tracks found for given PFParticle (after checking it was a track). Skipping this PFParticle but take note...";
-      continue;
-    }
-    double trkLength = pfpTrk.at(0)->Length();
+    // FOR PFP NUs, loop through the daughters and require at least one track of >50cm (could also try 2...)
+    for( auto const& iDaughter : iPfp->Daughters() ){
+      // TODO: Probably want to add a check and a throw here if it can't find the daughter (not just an error or warning, 
+      //since that would not be good I think). See how it was implemented in the bool lar_pandora::LArPandoraHelper::IsFinalState function in doxygem
+      art::Ptr< recob::PFParticle > iPfpDaughter = pfpMap.find(iDaughter)->second;
 
-    std::cout << "length = " << trkLength
-              << " ... Dir X = " << pfpTrk.at(0)->StartDirection().Unit().X()
-              << " ... Dir Y = " << pfpTrk.at(0)->StartDirection().Unit().Y() << std::endl;
+      // debug print out useful to look at pfparticle hierarchies
+      mf::LogDebug("SimpleScanFilter") << "PFParticle Child with ID: " << iDaughter << "(assumption check: " << iPfpDaughter.key() << ", " << iPfpDaughter->Self() << ")" << "\n"
+				       << "                     PDG: " << iPfpDaughter->PdgCode();
 
-    if( !fCompleteLoop && trkLength>50. ) return true; // if not making debug plots and we found our criteria, then go!
-    if( fCompleteLoop ){
-      trkLengthAll->Fill(trkLength);
-      trkLengthAllX->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X()) );
-      trkLengthAllY->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().Y()) );
-      if( trkLength > maxTrkLength ){
-	maxTrkLength = trkLength;
-	maxTrkLengthX = trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X());
+      if( abs(iPfpDaughter->PdgCode())!=13 ) continue; // skip non-track PFPs for now
+
+      //////////////////////////////////////////
+      // NOTE: When looping through tracks only and not the PFP Nus, then the following 
+      //       lines should be back with the main loop through PFParticles
+      //// START
+      //////////////////////////////////////////
+
+      // CHECK TRACK
+      // -------------------
+      // access the associated track, if PFParticle's PDG Code == +/- 13
+      std::vector< art::Ptr<recob::Track> > pfpTrk = fmtrk.at(iPfpDaughter.key()); // for putting back in main pfp loop, iPfpDaughter.key() -> iPfp.key()
+      if( pfpTrk.size()!=1 ){
+	mf::LogWarning("SimpleScanFilter") << "Zero/multiple tracks (" << pfpTrk.size() << ") found for given PFParticle (after checking it was a track). Skipping this PFParticle but take note...";
+	continue;
       }
-    }
-  }
+
+      // debug print out useful to look at pfparticle hierarchies
+      mf::LogDebug("SimpleScanFilter") << "PFParticle Child with ID: " << iDaughter << "(assumption check: " << iPfpDaughter.key() << ")" << "\n"
+				       << "            Track Length: " << pfpTrk.at(0)->Length();
+
+      // check track containment -- currently hard coded 2cm outside boundaries
+      if( fContainTracks ){
+	double trkStartX = pfpTrk.at(0)->Start().X();
+	double trkEndX = pfpTrk.at(0)->End().X();
+	double trkStartY = pfpTrk.at(0)->Start().Y();
+	double trkEndY = pfpTrk.at(0)->End().Y();
+	double trkStartZ = pfpTrk.at(0)->Start().Z();
+	double trkEndZ = pfpTrk.at(0)->End().Z();
+
+	// check start
+	if( !( (tpcContXMax-trkStartX > fTolContainTrack) && (trkStartX-tpcContXMin > fTolContainTrack) && 
+	       (tpcContYMax-trkStartY > fTolContainTrack) && (trkStartY-tpcContYMin > fTolContainTrack) &&
+	       (tpcContZMax-trkStartZ > fTolContainTrack) && (trkStartZ-tpcContZMin > fTolContainTrack) ) )
+	  continue;
+	// check end
+	if( !( (tpcContXMax-trkEndX > fTolContainTrack) && (trkEndX-tpcContXMin > fTolContainTrack) &&
+	       (tpcContYMax-trkEndY > fTolContainTrack) && (trkEndY-tpcContYMin > fTolContainTrack) &&
+	       (tpcContZMax-trkEndZ > fTolContainTrack) && (trkEndZ-tpcContZMin > fTolContainTrack) ) )
+	  continue;
+      }
+
+      double trkLength = pfpTrk.at(0)->Length();
+
+      if( !fCompleteLoop && trkLength>50. ) return true; // if not making debug plots and we found our criteria, then go!
+      if( fCompleteLoop ){
+	trkLengthAll->Fill(trkLength);
+	trkLengthAllX->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X()) );
+	trkLengthAllY->Fill( trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().Y()) );
+	if( trkLength > maxTrkLengthVal ){
+	  maxTrkLengthVal  = trkLength;
+	  maxTrkLengthXVal = trkLength * fabs(pfpTrk.at(0)->StartDirection().Unit().X());
+	  maxTrkDirZVal    = pfpTrk.at(0)->StartDirection().Unit().Z();
+	}
+      }
+      //// END
+    } // end loop through daughters of this PFPNeutrino
+  } // end loop through PFPs
 
   // if doing fCompleteLoop then you should reach this point. Make one final decision about if the track is passing
   if( fCompleteLoop ){
-    trkMaxLength->Fill( maxTrkLength );
-    trkMaxLengthX->Fill( maxTrkLengthX );
-    //    if( maxTrkLength>50. ) return true;
+    trkMaxLength->Fill( maxTrkLengthVal );
+    trkMaxLengthX->Fill( maxTrkLengthXVal );
+    trkMaxDirZ->Fill( maxTrkDirZVal );
+    trkMaxLengthVsTrkMaxDirZ->Fill( maxTrkDirZVal, maxTrkLengthVal );
+    pfpNuCounter->Fill( pfpNus );
+    //    if( maxTrkLengthVal>50. ) return true;
   }
 
   ///////////////////////////////
