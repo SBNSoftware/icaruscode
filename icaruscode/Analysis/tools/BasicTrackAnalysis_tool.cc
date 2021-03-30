01522c12207dcde19c48e7247b3fd5dbcaede9e5
Filippo Varanini <varanini@icarusgpvm04.fnal.gov>
2021-03-30 16:08:02 -0500

    trackhitanaSN modules

--- icaruscode/Analysis/tools/BasicTrackAnalysis_tool.cc
+++ icaruscode/Analysis/tools/BasicTrackAnalysis_tool.cc
@@ -13,6 +13,7 @@
 #include "larcore/Geometry/Geometry.h"
 
 #include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RawData/RawDigit.h"
 
 #include "TH1.h"
 #include "TH2.h"
@@ -83,7 +84,7 @@ public:
      *  @brief Interface for filling histograms
      */
     void fillHistograms(const art::Event&)  const override;
-    
+    void fillHistograms (const art::Ptr<recob::Track>&) const;
 private:
     
     // Fcl parameters.
@@ -92,7 +93,13 @@ private:
     
     // Pointers to the histograms we'll create.
     TH1D*     fHitsByWire[3];
-    TH2D*     fPulseHVsWidth[3];
+TH1D*     fHeight[3];
+TH1D*     fSumadc[3];
+
+TH1D*     fHeight3mm[3];
+TH1D*     fPitch[3];
+    TH2D*     fPulseHVsDx[3];
+TH2D*     fSumadcVsDx[3];
     TProfile* fPulseHVsHitNo[3];
     
     // Useful services, keep copies for now (we can update during begin run periods)
@@ -130,7 +137,8 @@ BasicTrackAnalysis::~BasicTrackAnalysis()
 ///
 void BasicTrackAnalysis::configure(fhicl::ParameterSet const & pset)
 {
-    fTrackProducerLabelVec = pset.get<std::vector<art::InputTag>>("TrackProducerLabel",   std::vector<art::InputTag>()={""});
+    fTrackProducerLabelVec = pset.get<std::vector<art::InputTag>>("TrackProducerLabel",   std::vector<art::InputTag>());
+//fTrackProducerLabelVec = pset.get<std::vector<art::InputTag>>("TrackProducerLabel",   std::vector<art::InputTag>()={"pandoraTrackICARUS"});
     fLocalDirName          = pset.get<std::string               >(  "LocalDirName",       std::string("wow"));
 }
 
@@ -145,23 +153,42 @@ void BasicTrackAnalysis::initializeHists(art::ServiceHandle<art::TFileService>& 
     fHitsByWire[1]    = dir.make<TH1D>("HitsByWire1", ";Wire #", fGeometry->Nwires(1), 0., fGeometry->Nwires(1));
     fHitsByWire[2]    = dir.make<TH1D>("HitsByWire2", ";Wire #", fGeometry->Nwires(2), 0., fGeometry->Nwires(2));
     
-    fPulseHVsWidth[0] = dir.make<TH2D>("PHVsWidth0",    ";PH;Width", 100,  0.,  100., 100,  0., 20.);
-    fPulseHVsWidth[1] = dir.make<TH2D>("PHVsWidth1",    ";PH;Width", 100,  0.,  100., 100,  0., 20.);
-    fPulseHVsWidth[2] = dir.make<TH2D>("PHVsWidth2",    ";PH;Width", 100,  0.,  100., 100,  0., 20.);
+    fPulseHVsDx[0] = dir.make<TH2D>("PHVsDx0",    ";Dx;PH", 50, 0.3,0.8,150,0.,150.);
+    fPulseHVsDx[1] = dir.make<TH2D>("PHVsDx1",    ";Dx;PH", 50, 0.3,0.8,150,0.,150.);
+    fPulseHVsDx[2] = dir.make<TH2D>("PHVsDx2",    ";Dx;PH", 50, 0.3,0.8, 150,0.,150.);
+fSumadcVsDx[0] = dir.make<TH2D>("SumadcVsDx0",    ";Dx;Area", 50, 0.3,0.8,150,0.,1500.);
+    fSumadcVsDx[1] = dir.make<TH2D>("SumadcVsDx1",    ";Dx;Area", 50, 0.3,0.8,150,0.,1500.);
+    fSumadcVsDx[2] = dir.make<TH2D>("SumadcVsDx2",    ";Dx;Area", 50, 0.3,0.8, 150,0.,1500.);
     
-    fPulseHVsHitNo[0] = dir.make<TProfile>("PHVsNo0",   ";Hit #;PH", 1000, 0., 1000., 0., 100.);
-    fPulseHVsHitNo[1] = dir.make<TProfile>("PHVsNo1",   ";Hit #;PH", 1000, 0., 1000., 0., 100.);
-    fPulseHVsHitNo[2] = dir.make<TProfile>("PHVsNo2",   ";Hit #;PH", 1000, 0., 1000., 0., 100.);
+    fPulseHVsHitNo[0] = dir.make<TProfile>("PHVsNo0",   ";Hit #;PH", 50, 0., 5., 0., 100.);
+    fPulseHVsHitNo[1] = dir.make<TProfile>("PHVsNo1",   ";Hit #;PH", 50, 0., 5., 0., 100.);
+    fPulseHVsHitNo[2] = dir.make<TProfile>("PHVsNo2",   ";Hit #;PH", 50, 0., 5., 0., 100.);
 
+    fHeight[0]    = dir.make<TH1D>("Height0", ";PH", 150,0.,150.);
+    fHeight[1]    = dir.make<TH1D>("Height1", ";PH", 150,0.,150.);
+    fHeight[2]    = dir.make<TH1D>("Height2", ";PH", 150,0.,150.);
+ fHeight3mm[0]    = dir.make<TH1D>("Height3mm0", ";PH", 150,0.,150.);
+    fHeight3mm[1]    = dir.make<TH1D>("Height3mm1", ";PH", 150,0.,150.);
+    fHeight3mm[2]    = dir.make<TH1D>("Height3mm2", ";PH", 150,0.,150.);
+fPitch[0]    = dir.make<TH1D>("pitch0", ";Dx", 50,0.,5.);
+    fPitch[1]    = dir.make<TH1D>("pitch1", ";Dx", 50,0.,5.);
+    fPitch[2]    = dir.make<TH1D>("pitch2", ";Dx", 50,0.,5.);
+fSumadc[0]    = dir.make<TH1D>("Sumadc0", ";Area", 150,0.,1500.);
+    fSumadc[1]    = dir.make<TH1D>("Sumadc1", ";Area", 150,0.,1500.);
+    fSumadc[2]    = dir.make<TH1D>("Sumadc2", ";Area", 150,0.,1500.);
     return;
 }
     
 void BasicTrackAnalysis::fillHistograms(const art::Event& event) const
 {
+std::cout << " test filling " << std::endl;
+art::ServiceHandle<geo::Geometry const> geom;
+//fHitsByWire[0]->Fill(0);
     // The game plan for this module is to look at recob::Tracks and objects associated to tracks
     // To do this we need a valid track collection for those we are hoping to look at
     for(const auto& trackLabel : fTrackProducerLabelVec)
     {
+std::cout << " filling track histos " << trackLabel <<std::endl;
         art::Handle<std::vector<recob::Track> > trackHandle;
         event.getByLabel(trackLabel, trackHandle);
         
@@ -169,18 +196,70 @@ void BasicTrackAnalysis::fillHistograms(const art::Event& event) const
         {
             // Recover the collection of associations between tracks and hits
             art::FindManyP<recob::Hit> trackHitAssns(trackHandle, event, trackLabel);
-            
+
             for(size_t trackIdx = 0; trackIdx < trackHandle->size(); trackIdx++)
             {
+           std::vector<art::Ptr<recob::Hit>> trkHits = trackHitAssns.at(trackIdx);
+
                 art::Ptr<recob::Track> track(trackHandle,trackIdx);
-                
+                std::cout << " track " << trackIdx << " hits " <<trkHits.size() << std::endl;
+geo::Vector_t track_dir = track->DirectionAtPoint(0);
+std::cout << " track dir " << track_dir.X() << " " << track_dir.Y() << " " << track_dir.Z()<< std::endl;
+float norm=sqrt(track_dir.X()*track_dir.X()+track_dir.Y()*track_dir.Y()+track_dir.Z()*track_dir.Z());
+std::cout << " norm " << norm << std::endl;
+                for(long unsigned int jh=0;jh<trkHits.size();jh++) {
+auto hitPtr=trkHits[jh];
+
+ const geo::WireID& wireID   = hitPtr->WireID();
+        /*float              chi2DOF  = std::min(hitPtr->GoodnessOfFit(),float(249.8));
+        int                numDOF   = hitPtr->DegreesOfFreedom();*/
+        int                hitMult  = hitPtr->Multiplicity();
+        /*float              peakTime = std::min(float(3199.),hitPtr->PeakTime());
+        float              charge   = hitPtr->Integral();*/
+        float              sumADC   = hitPtr->SummedADC();
+        float              hitPH    = hitPtr->PeakAmplitude();
+        //float              hitSigma = hitPtr->RMS();
+        
+        size_t             plane    = wireID.Plane;
+       // size_t             wire     = wireID.Wire;
+//std::cout << " amplitude " << hitPH << std::endl;
+      
+ double angleToVert = geom->WireAngleToVertical(hitPtr->View(), hitPtr->WireID().TPC, hitPtr->WireID().Cryostat) - 0.5*::util::pi<>();
+double cosgamma = std::abs(std::sin(angleToVert)*track_dir.Y() + std::cos(angleToVert)*track_dir.Z());
+std::cout << " plane " << plane <<  " view " << hitPtr->View() << " atv " << angleToVert << std::endl;
+ 
+  double pitch;
+  if (cosgamma) {
+    pitch = geom->WirePitch(hitPtr->View())/cosgamma;
+  }
+  else {
+    pitch = 0.;
+  }
+
+std::cout <<  " plane " << plane << " cosgamma "<< cosgamma << " pitch " << pitch << std::endl;
+if(hitMult==1) {
+fHeight3mm[plane]->Fill(hitPH*(0.3/pitch));
+fHeight[plane]->Fill(hitPH);
+fPitch[plane]->Fill(pitch);
+fSumadc[plane]->Fill(sumADC);
+fPulseHVsDx[plane]->Fill(pitch,hitPH);
+fSumadcVsDx[plane]->Fill(pitch,sumADC);
+}
+}
             }
         }
     }
 
     return;
 }
-    
+
+void BasicTrackAnalysis::fillHistograms( const art::Ptr<recob::Track>& trk) const
+{
+ 
+
+    return;
+}
+
 // Useful for normalizing histograms
 void BasicTrackAnalysis::endJob(int numEvents)
 {
