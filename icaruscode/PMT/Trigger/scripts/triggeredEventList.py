9db07565d91c1de6834843f975296a88ecad7030
Gianluca Petrillo <petrillo@slac.stanford.edu>
2021-08-04 17:51:33 -0500

    triggeredEventList.py can provide more info on event location

--- icaruscode/PMT/Trigger/scripts/triggeredEventList.py
+++ icaruscode/PMT/Trigger/scripts/triggeredEventList.py
@@ -5,7 +5,7 @@ import time
 __doc__ = """Prints the list of events passing the specified trigger."""
 __author__ = "Gianluca Petrillo (petrillo@slac.stanford.edu)"
 __date__ = time.strptime("July 11, 2021", "%B %d, %Y")
-__version__ = "1.0"
+__version__ = "1.1"
 
 import sys
 import galleryUtils
@@ -21,25 +21,35 @@ class EventTagCache:
   def __init__(self,
                run=False, subRun=False, event=True, sourceFile=False,
                human=False, short=False,
+               trigTime=False,
+               separator=" ",
                ):
     self.eventTag = None
     self.run = run
     self.subrun = subRun
     self.event = event
+    self.trigTime = trigTime
     self.sourceFile = sourceFile
     self.human = human
     self.short = short
+    self.separator = separator
   # __init__()
   
   def prepareFor(self, event):
     self.eventObj = event
     self.eventTag = None
   
-  def __call__(self, event = None):
+  def __call__(self, trigInfo, event = None):
     if self.eventTag is None or event is not self.eventObj:
       self.eventTag = self._makeTag(event)
-    return self.eventTag
+    return self.addTriggerInfo(self.eventTag, trigInfo)
   # __call__()
+  
+  def addTriggerInfo(self, tag, trigInfo):
+    info = [ tag, ]
+    if self.trigTime: info.append(f"{trigInfo['trigTime']:.3f}") # to nanosecond
+    return self.separator.join(info)
+  # addTriggerInfo()
   
   def _makeTag(self, event=None):
     if event is not None: self.eventObj = event
@@ -74,13 +84,58 @@ class EventTagCache:
         eventID.append(repr(fileName))
         eventID.append(str(eventInFile))
     # if print event
-    return " ".join(eventID)
+    return self.separator.join(eventID)
   # _makeTag()
   
 # class EventTagCache
 
 
+class IntervalClass:
+  """Simple numeric range class (usual [ lower, upper [ interval).
+  
+  Boundaries set to `None` are ignored.
+  """
+  
+  def __init__(self, lower, upper):
+    self.lower = lower
+    self.upper = upper
+  def contains(self, value):
+    if self.lower is not None and value < self.lower: return False
+    if self.upper is not None and value >= self.upper: return False
+    return True
+  def __contains__(self, value): return self.contains(value)
+  def __nonzero__(self): return self.lower is not None or self.upper is not None
+  def __str__(self):
+    if self.lower is None:
+      if self.upper is None: return "(any)"
+      else: return f" < {self.upper}"
+    else:
+      if self.upper is None: return f">= {self.lower}"
+      else: return f"{self.lower} -- {self.upper}"
+  # __str__()
+# class IntervalClass
+
+
 # ------------------------------------------------------------------------------
+def analyzeTrigger(eventTag, triggers, trigSpec, args):
+  if len(triggers) == 0: return False
+  
+  # if this is expensive (which should not be since the trigger data product
+  # has already been read) it can be made conditional based on `args.timeRange`.
+  trigTime = triggers[0].TriggerTime()
+  if trigTime not in args.timeRange: return False
+  
+  trigSpec['count'] += 1
+  
+  if args.noList: return True # if no printing is requested, skip the rest
+  
+  trigInfo = { 'trigTime': trigTime, }
+  print(eventTag(trigInfo=trigInfo), file=trigSpec['dest'])
+  
+  return True
+# analyzeTrigger()
+
+
 def findTriggeredEvents(sampleEvents, triggers, args):
   
   # digest the trigger info options
@@ -111,7 +166,9 @@ def findTriggeredEvents(sampleEvents, triggers, args):
       sourceFile=args.sourcefile,
       human=args.human,
       short=args.short,
-      )
+      trigTime=args.trigtime,
+      separator=args.sep,
+      ) if args.doList else None
     
     for iEvent, event in enumerate(galleryUtils.forEach(sampleEvents)):
       
@@ -119,13 +176,11 @@ def findTriggeredEvents(sampleEvents, triggers, args):
         logging.warning(f"Limit of {iEvent} events reached: stopping now.")
         break
       
-      eventTag.prepareFor(event)
+      if eventTag: eventTag.prepareFor(event)
       
       for trigSpec in trigSpecs:
         triggers = getTrigger(trigSpec['tag']).product()
-        if len(triggers) == 0: continue
-        print(eventTag(), file=trigSpec['dest'])
-        trigSpec['count'] += 1
+        analyzeTrigger(eventTag, triggers, trigSpec, args)
       # for
     
     # for all events
@@ -182,6 +237,34 @@ def buildDefaultOutputFile(spec):
 # buildDefaultOutputFile()
 
 
+def parseTimeRange(rangeSpec, sep = "/"):
+  
+  if not rangeSpec: return IntervalClass(None, None)
+  try:
+    lower, upper = rangeSpec.split(sep)
+  except ValueError:
+    raise RuntimeError(f"Invalid format for time range: {rangeSpec}")
+  
+  if lower:
+    try: lower = float(lower)
+    except ValueError:
+      RuntimeError(
+        "Lower bound of time range '{rangeSpec}' ('f{lower}')"
+        " is not a valid time."
+        )
+  else: lower = None
+  if upper:
+    try: upper = float(upper)
+    except ValueError:
+      RuntimeError(
+        "Upper bound of time range '{rangeSpec}' ('f{upper}')"
+        " is not a valid time."
+        )
+  else: upper = None
+  return IntervalClass(lower, upper)
+# parseTimeRange()
+
+
 if __name__ == "__main__":
   
   # --- BEGIN -- argument parsing  ---------------------------------------------
@@ -196,7 +279,7 @@ if __name__ == "__main__":
      f" or just instance name (`M5S10`, becoming `{DefaultTriggerModuleLabel}:M5S10`)"
      )
   
-  # options
+  # input options
   inputGroup = parser.add_argument_group("Input options")
   inputGroup.add_argument("--maxEvents", "-n", type=int,
     help="do not process more that this number of events")
@@ -209,6 +292,15 @@ if __name__ == "__main__":
     help="like `--all`, redirecting each trigger into its default-named file"
     )
   
+  # processing options
+  filterGroup = parser.add_argument_group("Filtering options")
+  filterGroup.add_argument("--accept-time", "-T", dest="acceptTime", type=str,
+    help="only consider triggers in the specified range of time: `<min>:<max>`;"
+         " time is in electronics time scale, microseconds;"
+         " either `<min>` or `<max>` may be omitted"
+    )
+  
+  # output options
   outputFormatGroup = parser.add_argument_group("Output format")
   outputFormatGroup.add_argument("--run", "-r", action="store_true",
     help="print the run number for triggering events [%(default)s]")
@@ -216,6 +308,8 @@ if __name__ == "__main__":
     help="print the subrun number for triggering events [%(default)s]")
   outputFormatGroup.add_argument("--event", "-e", action="store_false",
     help="print the event number for triggering events [%(default)s]")
+  outputFormatGroup.add_argument("--trigtime", "-t", action="store_true",
+    help="print the relative time of first trigger [%(default)s]")
   outputFormatGroup.add_argument("--sourcefile", "--file", "-f",
     action="store_true",
     help="print the file where triggering events are stored [%(default)s]"
@@ -224,7 +318,12 @@ if __name__ == "__main__":
     help="print the event ID with extended labels [%(default)s]")
   outputFormatGroup.add_argument("--short", "-S", action="store_true",
     help="print the event ID with short labels [%(default)s]")
+  outputFormatGroup.add_argument("--nolist", "-N", dest="noList",
+    action="store_true", help="do not print any event ID (only summary)")
+  outputFormatGroup.add_argument("--separator", dest="sep", default=" ",
+    help="use this string as separator [%(default)r]")
 
+  # "modal" options
   generalOptGroup = parser.add_argument_group("General options")
   generalOptGroup.add_argument("--list", "-l", dest="doList", action="store_true",
     help="prints the available trigger data product tags, and exits")
@@ -244,7 +343,16 @@ if __name__ == "__main__":
   
   sampleEvents = galleryUtils.makeEvent(args.InputFile)
   
+  hasTriggerSpecs = args.TriggerSpec or args.all or args.allToFiles
+  
+  if not args.doList and not hasTriggerSpecs:
+    listTriggerDataProducts(sampleEvents)
+    logging.error("No trigger tag specified!")
+    sys.exit(1)
+  
   if args.doList: sys.exit(listTriggerDataProducts(sampleEvents))
+  
+  args.timeRange = parseTimeRange(args.acceptTime)
   
   # process trigger specifications
   triggerSpecs = []
