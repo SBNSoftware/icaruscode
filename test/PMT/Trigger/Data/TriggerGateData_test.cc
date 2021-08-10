aaa8331e6d5edfd0f779740d481ee8e63413f17c
Lynn Garren <garren@fnal.gov>
2021-08-10 09:43:14 -0500

    updates for boost v1_75_0

--- test/PMT/Trigger/Data/TriggerGateData_test.cc
+++ test/PMT/Trigger/Data/TriggerGateData_test.cc
@@ -17,8 +17,7 @@
 
 // Boost libraries
 #define BOOST_TEST_MODULE ( TriggerGateData_test )
-#include <cetlib/quiet_unit_test.hpp> // BOOST_AUTO_TEST_CASE()
-#include <boost/test/test_tools.hpp> // BOOST_CHECK(), BOOST_CHECK_EQUAL()
+#include <boost/test/unit_test.hpp>
 
 
 // -----------------------------------------------------------------------------
