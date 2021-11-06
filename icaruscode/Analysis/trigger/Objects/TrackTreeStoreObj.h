ee88123cdd44903f024c7dd7ea526d6b52720375
Gianluca Petrillo <petrillo@slac.stanford.edu>
2021-11-05 21:42:35 -0500

    TrackTreeStoreObj.h: def. construction via member initializer

--- icaruscode/Analysis/trigger/Objects/TrackTreeStoreObj.h
+++ icaruscode/Analysis/trigger/Objects/TrackTreeStoreObj.h
@@ -1,61 +1,37 @@
 #ifndef SBN_TRACKTREESTOREOBJ_H
 #define SBN_TRACKTREESTOREOBJ_H
 
+#include "cstdint" // std::uint64_t
+
 namespace sbn {
 
   struct selTrackInfo {
-    int trackID;
-    float t0;
-    float start_x;
-    float start_y;
-    float start_z;
-    float end_x;
-    float end_y;
-    float end_z;
-    float length;
-    float dir_x;
-    float dir_y;
-    float dir_z;
-  selTrackInfo():
-    trackID(-1),
-      t0(-1),
-      start_x(-1),
-      start_y(-1),
-      start_z(-1),
-      end_x(-1),
-      end_y(-1),
-      end_z(-1),
-      length(-1),
-      dir_x(-1),
-      dir_y(-1),
-      dir_z(-1)
-    {}
-  };
+    int trackID = -1;
+    float t0 = -1.0;
+    float start_x = -1.0;
+    float start_y = -1.0;
+    float start_z = -1.0;
+    float end_x = -1.0;
+    float end_y = -1.0;
+    float end_z = -1.0;
+    float length = -1.0;
+    float dir_x = -1.0;
+    float dir_y = -1.0;
+    float dir_z = -1.0;
+  };  // selTrackInfo
 
   struct selBeamInfo {
-    uint64_t beamGateSimStart;
-    float beamGateDuration;
-    unsigned int beamGateType;
-  selBeamInfo():
-    beamGateSimStart(0),
-      beamGateDuration(-1),
-    beamGateType(999)
-    {}
+    std::uint64_t beamGateSimStart = 0;
+    float beamGateDuration = -1.0;
+    unsigned int beamGateType = 999;
   };
   
   struct selTriggerInfo {
-    unsigned int beamType;
-    uint64_t triggerTime;
-    uint64_t beamGateTime;
-    uint64_t triggerID;
-    uint64_t gateID;
-  selTriggerInfo():
-    beamType(0),
-    triggerTime(0),
-      beamGateTime(0),
-      triggerID(0),
-      gateID(0)
-    {}
+    unsigned int beamType = 0;
+    std::uint64_t triggerTime = 0;
+    std::uint64_t beamGateTime = 0;
+    std::uint64_t triggerID = 0;
+    std::uint64_t gateID = 0;
   };
   
 } // namespace sbn
