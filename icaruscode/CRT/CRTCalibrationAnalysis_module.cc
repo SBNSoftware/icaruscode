0af6ce0116e1e45cb506b4ea5e3b8a978a8e37fb
Tyler Boone <tboone@icarusgpvm02.fnal.gov>
2022-03-04 10:11:18 -0600

    modified:   icaruscode/CRT/CRTCalibrationAnalysis_module.cc

--- icaruscode/CRT/CRTCalibrationAnalysis_module.cc
+++ icaruscode/CRT/CRTCalibrationAnalysis_module.cc
@@ -68,34 +68,6 @@ using std::to_string;
 namespace icarus {
 namespace crt {
 
-  //-----------------------------------------------------------------------
-  //-----------------------------------------------------------------------
-  // class definition
-  /**
-   * @brief Example analyzer
-   * 
-   * This class extracts information from the generated and reconstructed
-   * particles.
-   *
-   * It produces histograms for the simulated particles in the input file:
-   * - PDG ID (flavor) of all particles
-   * - momentum of the primary particles selected to have a specific PDG ID
-   * - length of the selected particle trajectory
-   * 
-   * It also produces two ROOT trees.
-   *
-   * The first ROOT tree contains information on the simulated
-   * particles, including "dEdx", a binned histogram of collected
-   * charge as function of track range.
-   * 
-   * Configuration parameters
-   * =========================
-   * 
-   * - *SimulationLabel* (string, default: "largeant"): tag of the input data
-   *   product with the detector simulation information (typically an instance
-   *   of the LArG4 module)
-   *
-   */
   class CRTCalibrationAnalysis : public art::EDAnalyzer
   {
   public:
@@ -116,82 +88,24 @@ namespace crt {
     
     using Parameters = art::EDAnalyzer::Table<Config>;
     
-    // -------------------------------------------------------------------
-    // -------------------------------------------------------------------
-    // Standard constructor for an ART module with configuration validation;
-    // we don't need a special destructor here.
-
+  
     /// Constructor: configures the module (see the Config structure above)
     explicit CRTCalibrationAnalysis(Parameters const& config);
 
     virtual void beginJob() override;
     virtual void beginRun(const art::Run& run) override;
     virtual void analyze (const art::Event& event) override;
-//    virtual void endJob() override;
+
 
   private:
 
     art::ServiceHandle<art::TFileService> tfs;
 
-    TTree* fDAQNtuple;
-
     map<uint8_t,vector<TH1F*>*> macToHistos;
-
-    void FillFebMap();
- //   TTree *calTree;
 
     // The parameters we'll read from the .fcl file.
     art::InputTag fCRTDAQProducerLabel;
 
-    static map<int, vector<pair<int,int>>> fFebMap;
-
-    // The n-tuples we'll create.
-
-    // The comment lines with the @ symbols define groups in doxygen. 
-    /// @name The variables that will go into both n-tuples.
-    /// @{
-    int fEvent;        ///< number of the event being processed
-    int fRun;          ///< number of the run being processed
-    int fSubRun;       ///< number of the sub-run being processed
-    /// @}
-
-    /// @name The variables that will go into the CosmicDisplay n-tuple.
-    /// @{
-    static const int LAR_PROP_DELAY = 1.0/(30.0/1.38); //[ns/cm]
-
-    //CRT data product vars
-    //static const int kDetMax = 64;
-    int      fDetEvent;
-    int      fNChan; ///< number of channels above threshold for this front-end board readout
-    int      fEntry; ///< front-end board entry number (reset for each event)
-    int      fFEBReg; ///< CRT region for this front-end board
-    int      fMac5; ///< Mac5 address for this front-end board
-    int      fDetSubSys;
-    double   fT0;///< signal time w.r.t. global event time
-    double   fT1;///< signal time w.r.t. PPS
-    int      fADC[64];///< signal amplitude
-    vector<vector<int>> fTrackID;///< track ID(s) of particle that produced the signal
-    vector<vector<int>> fDetPDG; /// signal inducing particle(s)' PDG code
-
-    //CRT hit product vars
-//    int      fHitEvent;
-//    float    fXHit; ///< reconstructed X position of CRT hit (cm)
-//    float    fYHit; ///< reconstructed Y position of CRT hit (cm)
-//    float    fZHit; ///< reconstructed Z position of CRT hit (cm)
-//    float    fXErrHit; ///< stat error of CRT hit reco X (cm)
-//    float    fYErrHit; ///< stat error of CRT hit reco Y (cm)
-//    float    fZErrHit; ///< stat error of CRT hit reco Z (cm)
-//    int32_t    fT0Hit; ///< hit time w.r.t. global event time
-//    int32_t    fT1Hit; ///< hit time w.r.t. PPS
-//    int       fHitReg; ///< region code of CRT hit
-//    int       fHitSubSys;
-//    int       fNHit; ///< number of CRT hits for this event
-//    int       fHitStrip;
-//    int       fHitMod;
-//    int       fNHitFeb;
-//    float     fHitTotPe;
-
-    
     // Other variables that will be shared between different methods.
     geo::GeometryCore const* fGeometryService;   ///< pointer to Geometry provider
     int                      fTriggerOffset;     ///< (units of ticks) time of expected neutrino event
@@ -200,19 +114,7 @@ namespace crt {
 
 
   //-----------------------------------------------------------------------
-  //-----------------------------------------------------------------------
-  // class implementation
-
-  //-----------------------------------------------------------------------
-  // Constructor
-  // 
-  // Note that config is a Table<Config>, and to access the Config
-  // value we need to use an operator: "config()". In the same way,
-  // each element in Config is an Atom<Type>, so to access the type we
-  // again use the call operator, e.g. "SimulationLabel()".
-
-  map<int,vector<pair<int,int>>> CRTCalibrationAnalysis::fFebMap;
- 
+   
   CRTCalibrationAnalysis::CRTCalibrationAnalysis(Parameters const& config)
     : EDAnalyzer(config)
     , fCRTDAQProducerLabel(config().CRTDAQLabel())
@@ -225,56 +127,23 @@ namespace crt {
     auto const clockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataForJob();
     fTriggerOffset = trigger_offset(clockData);
 
-	for(int i=1; i<94; i++){
-
-		macToHistos[i] = new vector<TH1F*>();
-
-		for(int ch=0; ch<32; ch++){
-
-			string hname = "hadc_"+to_string(i)+"_"+to_string(ch);
-			string htitle = "raw charge: mac5 "+to_string(i)+", ch. "+to_string(ch);
-			macToHistos[i]->push_back(tfs->make<TH1F>(hname.c_str(),htitle.c_str(),4100,0,4100));
-//			macToHistos[i]->at(ch)->Write();
-		}
-	}
-  }
-
-  void CRTCalibrationAnalysis::FillFebMap() { 
-    if(!this->fFebMap.empty())
-        return;
-    std::string fullFileName;
-    cet::search_path searchPath("FW_SEARCH_PATH");
-    searchPath.find_file("feb_map.txt",fullFileName);
-    std::ifstream fin;
-    fin.open(fullFileName,std::ios::in);
-    if(fin.good()) std::cout << "opened file 'feb_map.txt' for reading..." << std::endl;
-    else //std::cout << "could not open file 'feb_map.txt' for reading!" << std::endl;
-        throw cet::exception("CRTDataAnalysis::FillFebMap") << "Unable to find/open file 'feb_map.txt'" << std::endl;
-    std::vector<std::string> row;
-    std::string line, word;
-    while(getline(fin,line)) {
-        row.clear();
-        std::stringstream s(line);
-        int mod;
-        while (std::getline(s, word, ',')) {
-            row.push_back(word);
-        }
-        mod = std::stoi(row[0]);
-        (this->fFebMap)[mod].push_back(std::make_pair(std::stoi(row[1]),std::stoi(row[2])));
-        if(row.size()>3)
-            (this->fFebMap)[mod].push_back(std::make_pair(std::stoi(row[3]),std::stoi(row[4])));
+    for(int i=1; i<94; i++){
+      
+      macToHistos[i] = new vector<TH1F*>();
+      
+      for(int ch=0; ch<32; ch++){
+	
+	string hname = "hadc_"+to_string(i)+"_"+to_string(ch);
+	string htitle = "raw charge: mac5 "+to_string(i)+", ch. "+to_string(ch);
+	macToHistos[i]->push_back(tfs->make<TH1F>(hname.c_str(),htitle.c_str(),4100,0,4100));
+      }
     }
-    std::cout << "filled febMap with " << (this->fFebMap).size() << " entries" << std::endl;
-    fin.close();
   }
-
   
   //-----------------------------------------------------------------------
   void CRTCalibrationAnalysis::beginJob()
   {
-    std::cout << " starting analysis job" << std::endl;
-
-}
+  }
    
   void CRTCalibrationAnalysis::beginRun(const art::Run& /*run*/)
   {
@@ -283,55 +152,30 @@ namespace crt {
   //-----------------------------------------------------------------------
   void CRTCalibrationAnalysis::analyze(const art::Event& event) 
   {
-    MF_LOG_DEBUG("CRT") << "beginning analyis" << '\n';
+    MF_LOG_DEBUG("CRTCalibrationAnalysis") << "beginning analyis" << '\n';
 
-    // Start by fetching some basic event information for our n-tuple.
-    fEvent  = event.id().event(); 
-    fRun    = event.run();
-    fSubRun = event.subRun();
-
-	std::cout << "event: " << fEvent << ", run " << fRun << ", subrun " << fSubRun << " found." << std::endl;
-
-    FillFebMap();//febMap);
 
     art::Handle<vector<icarus::crt::CRTData>> crtDAQHandle;
     bool isCRTDAQ = event.getByLabel(fCRTDAQProducerLabel, crtDAQHandle);
 
     if (isCRTDAQ)  {
-     std::cout << "about to loop over CRTDAQ entries" << std::endl;
-     for ( auto const& febdat : (*crtDAQHandle) ) {
-        fDetEvent       = fEvent;
-        fMac5           = febdat.fMac5;
-	if(fMac5>93) continue;
-	std::cout << "found mac5: " << fMac5 << std::endl;
-        fEntry          = febdat.fEntry;
-	fFEBReg         = fCrtutils->AuxDetRegionNameToNum(fCrtutils->MacToRegion(fMac5));
-        fNChan = 0;
-	fDetSubSys = fCrtutils->MacToTypeCode(fMac5);
-        fT0 = febdat.fTs0;
-        fT1 = febdat.fTs1;
+      MF_LOG_DEBUG("CRTCalibrationAnalysis") << "about to loop over CRTDAQ entries" << '\n';
 
-        int maxchan =0;
-        if(fDetSubSys!=2) maxchan=32;
-        else maxchan = 64;
-	std::cout << "maxchan = " << maxchan << std::endl;
-        for(int ch=0; ch<maxchan; ch++) {
-		std::cout << "channel " << ch << " found!" << std::endl;
-                fADC[ch] = febdat.fAdc[ch]; 
-		macToHistos[fMac5]->at(ch)->Fill( fADC[ch] );
+     for ( auto const& febdat : (*crtDAQHandle) ) {
+
+        for(int ch=0; ch<32; ch++) {
+	  macToHistos[febdat.fMac5]->at(ch)->Fill( febdat.fAdc[ch] );
         } 
 
-//        fDAQNtuple->Fill();
-
      } //for CRT FEB events
-
+     
     }//if crtdetsim products present
 
     else 
-	throw cet::exception("CRTSimAnalysis") << "CRTDAQ products not found!" << std::endl;
-
+      mf::LogError("CRTCalibrationAnalysis") << "CRTDAQ products not found!" << std::endl;
+    
   } // CRTCalibrationAnalysis::analyze()
- 
+  
   DEFINE_ART_MODULE(CRTCalibrationAnalysis)
 } // namespace crt
 } // namespace icarus
