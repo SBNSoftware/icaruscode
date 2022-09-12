860f241d916d7be7c498716682675b62a191685b
Gianluca Petrillo <petrillo@slac.stanford.edu>
2022-09-12 18:50:21 -0500

    Data product review: icarus::timing::PMTWaveformTimeCorrection
    * added documentation
    * added default values and magic constants
    * added a way to determine if the correction should be considered valid

--- icaruscode/Timing/DataProducts/PMTWaveformTimeCorrection.h
+++ icaruscode/Timing/DataProducts/PMTWaveformTimeCorrection.h
@@ -10,20 +10,39 @@
 #define ICARUSCODE_TIMING_DATAPRODUCTS_PMTWAVEFORMTIMECORRECTION_H
 
 // C/C++ standard libraries
-#include <iosfwd> // std::ostream
-#include <string>
-#include <array>
-#include <cassert>
+#include <limits>
 
 namespace icarus::timing{
 
+	/**
+	 * @brief Corrections to the PMT waveform time.
+	 * 
+	 * An (uncorrected) waveform expects that the trigger signal happened at the
+	 * nominal `TriggerTime()` from `detinfo::DetectorClocks`.
+	 * 
+	 * If that is not the case, the `startTime` correction can be used to shift
+	 * the waveform start time so that that assumption holds.
+	 * The new, corrected waveform is the old one _plus_ the correction.
+	 * 
+	 */
 	struct PMTWaveformTimeCorrection {
 		
-		unsigned int channelID;
+		/// Special value to denote no channel ID information (and no correction).
+		static constexpr auto NoChannelID = std::numeric_limits<unsigned int>::max();
 		
-		double sample;
+		/// The channel this correction was extracted from.
+		unsigned int channelID = NoChannelID;
+		
+		/// Time within the reference waveforms where the reference signal is found [ns]
+		double sample = std::numeric_limits<double>::lowest();
 
-		double startTime;
+		/// How earlier than the nominal trigger time the actual trigger signal
+		/// seems to come [us]
+		double startTime = 0.0;
+		
+		
+		/// Returns whether the correction values are valid.
+		bool isValid() const { return channelID != NoChannelID; }
 
 	};
 
