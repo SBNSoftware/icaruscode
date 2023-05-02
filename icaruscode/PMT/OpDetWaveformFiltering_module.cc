25f2b174e182c2b3a346fc4c9a9bad84e7446150
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2023-05-02 17:38:10 -0500

    some fixes + added standalone debug output tree

--- icaruscode/PMT/OpDetWaveformFiltering_module.cc
+++ icaruscode/PMT/OpDetWaveformFiltering_module.cc
@@ -8,7 +8,7 @@
 // framework libraries
 #include "canvas/Utilities/InputTag.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h" 
-#include "art/Framework/Core/SharedProducer.h"
+#include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
@@ -17,6 +17,7 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "fhiclcpp/types/Atom.h"
 #include "fhiclcpp/types/Sequence.h"
+#include "art_root_io/TFileService.h"
 
 // LArSoft libraries
 #include "lardataobj/RawData/OpDetWaveform.h"
@@ -24,11 +25,15 @@
 #include "lardataalg/DetectorInfo/DetectorTimings.h"
 #include "lardataalg/DetectorInfo/DetectorTimingTypes.h" // electronics_time
 
+// ROOT libraries
+#include "TTree.h"
+#include "TFile.h"
+
 // C/C++ standard libraries
 #include <memory> // std::unique_ptr<>
 #include <string>
 #include <utility> // std::move()
-
+#include <vector>
 
 // -----------------------------------------------------------------------------
 namespace icarus { class OpDetWaveformFiltering; }
@@ -66,18 +71,21 @@ namespace icarus { class OpDetWaveformFiltering; }
  *   from the laser, so `CorrectLaser` must also be set.
  * * `LogCategory` (string, default: `OpHitWaveformFiltering`): name of the
  *   message stream for console output.
- * 
+ * * `SaveDebug` (bool, default: `false`): create and save ROOT tree with debugging
+ *   information, with raw and filtered waveforms.
+ * * `OutputFile` (string, defaulf: `filter_debug.root`): name of the debug output
+ *   ROOT file, created only if `SaveDebug` is set to true.
  */
-class icarus::OpDetWaveformFiltering: public art::SharedProducer {
+class icarus::OpDetWaveformFiltering: public art::EDProducer {
   
 public:
   
   /// configuration of the module.
   struct Config {
 
-    fhicl::Sequence<art::InputTag> InputLabels {
-        fhicl::Name("InputLabels"), 
-        fhicl::Comment("list of the input labels to be used")
+    fhicl::Atom<art::InputTag> InputLabel {
+        fhicl::Name("InputLabel"), 
+        fhicl::Comment("Raw waveform input label to be used")
     };
 
     fhicl::Atom<double> TimeConstant {
@@ -92,44 +100,72 @@ public:
       "OpDetWaveformFiltering" // default
     };
     
+    fhicl::Atom<bool> SaveDebug {
+      fhicl::Name("SaveDebug"),
+      fhicl::Comment("save debugging info in ROOT tree"),
+      false // default
+    };
+    
+    fhicl::Atom<std::string> OutputFile {
+      fhicl::Name("OutputFile"),
+      fhicl::Comment("output ROOT tree filename"),
+      "filter_debug.root" // default
+    };
+    
   }; // struct Config
 
-  using Parameters = art::SharedProducer::Table<Config>;
+  using Parameters = art::EDProducer::Table<Config>;
   using nanoseconds = util::quantities::intervals::nanoseconds;
 
   /// constructor: just reads the configuration
-  explicit OpDetWaveformFiltering(Parameters const& config, art::ProcessingFrame const&);
+  explicit OpDetWaveformFiltering(Parameters const& config);
     
   /// process the event
-  void produce(art::Event& event, art::ProcessingFrame const&) override;
+  void produce(art::Event& event);
+  void beginJob();
+  void endJob();
 
   /// low pass discrete filter
   template<typename T> std::vector<T> lowPassFilter(std::vector<T> data, double dt, double RC) const;
 
 private:
 
-  std::vector<art::InputTag> const fInputLabels;
+  art::InputTag const fInputLabel;
   double const fTimeConstant; ///< RC time constant
   std::string const fLogCategory; ///< Category tag for messages.
+  bool const fSaveDebug;
+  std::string const fOutputFile;
+
+  TFile *fFile;
+  TTree *fTree;
+  int m_run;
+  int m_event;
+  int m_timestamp;
+  int m_channel_id;
+  double m_tstart;
+  int m_nsize;
+  std::vector<short> m_wf;
+  std::vector<short> m_filt_wf;
 
 };
 
 
 // -----------------------------------------------------------------------------
 icarus::OpDetWaveformFiltering::OpDetWaveformFiltering
-    ( Parameters const& config, art::ProcessingFrame const& )
-    : art::SharedProducer(config)
-    , fInputLabels{ config().InputLabels() }
+    ( Parameters const& config )
+    : art::EDProducer(config)
+    , fInputLabel{ config().InputLabel() }
     , fTimeConstant{ config().TimeConstant() }
     , fLogCategory{ config().LogCategory() }
+    , fSaveDebug{ config().SaveDebug() }
+    , fOutputFile{ config().OutputFile() }
 {
-    async<art::InEvent>();
     
     // configuration checks
-    if (fInputLabels.empty()) {
+    if (fInputLabel.empty()) {
         throw art::Exception{ art::errors::Configuration }
-          << "The list of input waveform data products ('"
-          << config().InputLabels.name() << "') is empty.\n";
+          << "The input waveform data product ('"
+          << config().InputLabel.name() << "') is empty.\n";
     }
     if (fTimeConstant < 0) {
         throw art::Exception{ art::errors::Configuration }
@@ -138,48 +174,71 @@ icarus::OpDetWaveformFiltering::OpDetWaveformFiltering
           << "') must be a positive number. Fix the configuration!\n";
     }
 
-    /// Consumes
-    for ( auto const & tag : fInputLabels )
-        consumes<std::vector<raw::OpDetWaveform>>(tag);
-
+    // consumes
+    consumes<std::vector<raw::OpDetWaveform>>(fInputLabel);
+	
+    // produces
     produces<std::vector<raw::OpDetWaveform>>();
 
 }
 
 // -----------------------------------------------------------------------------
-void icarus::OpDetWaveformFiltering::produce( art::Event& event, art::ProcessingFrame const& ) {
+
+void icarus::OpDetWaveformFiltering::beginJob() {
+
+  if(fSaveDebug) {
+    
+    fFile = new TFile(fOutputFile.c_str(),"RECREATE");
+    
+    fTree = new TTree("wftree", "waveform info" );
+    fTree->Branch("run",&m_run);
+    fTree->Branch("event",&m_event);
+    fTree->Branch("timestamp",&m_timestamp);
+    fTree->Branch("channel_id",&m_channel_id);
+    fTree->Branch("tstart",&m_tstart);
+    fTree->Branch("nsize",&m_nsize);
+    fTree->Branch("wf",&m_wf);
+    fTree->Branch("filt_wf",&m_filt_wf);
+  }
+}
+
+// -----------------------------------------------------------------------------
+void icarus::OpDetWaveformFiltering::produce( art::Event& event ) {
+
+    m_run = event.id().run();
+    m_event = event.id().event();
+    m_timestamp = event.time().timeHigh(); // precision to the second    
 
     // get optical detector sampling time
     detinfo::DetectorTimings const detTimings = detinfo::makeDetectorTimings(art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(event));
     nanoseconds opticalTick = detTimings.OpticalClockPeriod();
-    std::cout << "Tick [hopefully in ns] " << opticalTick.value() << std::endl;
-    std::cout << "Time constant [ns] " << fTimeConstant << std::endl;
 
     // create a copy of the waveforms 
     std::vector<raw::OpDetWaveform> filteredOpDetWfs;
+     
+    auto const& opDetWfs = event.getProduct<std::vector<raw::OpDetWaveform>>(fInputLabel);
 
-    for(art::InputTag const& label: fInputLabels) {
-        
-        auto const& opDetWfs = event.getProduct<std::vector<raw::OpDetWaveform>>(label);
+    for(  auto const & wf : opDetWfs  ){
 
-        for(  auto const & wf : opDetWfs  ){
+    	m_channel_id = wf.ChannelNumber();
+        m_tstart = wf.TimeStamp();
+        m_wf = wf.Waveform();
 
-            double channel = wf.ChannelNumber();
-            double time = wf.TimeStamp();
+	// filtering waveform data
+	m_filt_wf = lowPassFilter( m_wf, opticalTick.value(), fTimeConstant);	      
+        std::vector<uint16_t> filtwf( m_filt_wf.begin(), m_filt_wf.end());
 
-	    // filtering waveform data
-	    std::vector<raw::ADC_Count_t> filt = lowPassFilter( wf.Waveform(), opticalTick.value(), fTimeConstant);	      
-            std::vector<uint16_t> filtwf( filt.begin(), filt.end());
+	// fill new collection
+	filteredOpDetWfs.emplace_back(
+           m_tstart,           // timestamp
+           m_channel_id,       // channel
+           filtwf              // waveform
+        );
+	
+	if(fSaveDebug) fTree->Fill();
 
-	    // fill new collection
-	    filteredOpDetWfs.emplace_back(
-                time,               // timestamp
-                channel,            // channel
-                filtwf              // waveform
-            );
-        }
     }
-
+ 
     // the filtered waveforms are also saved in the event stream
     event.put(
       std::make_unique<std::vector<raw::OpDetWaveform>>(std::move(filteredOpDetWfs)) 
@@ -201,6 +260,15 @@ template<typename T> std::vector<T> icarus::OpDetWaveformFiltering::lowPassFilte
 
     return out;
 }
+
+
+void icarus::OpDetWaveformFiltering::endJob()
+{
+    fFile->cd();
+    fTree->Write();
+    fFile->Close();
+}
+
 
 // -----------------------------------------------------------------------------
 DEFINE_ART_MODULE(icarus::OpDetWaveformFiltering)
