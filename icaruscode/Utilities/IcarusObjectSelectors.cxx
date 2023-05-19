e4844236152a353f8eaa4cb4d6e5909043ce7ea3
Gianluca Petrillo <petrillo@slac.stanford.edu>
2023-05-19 12:24:42 -0500

    Updated icarus::crt::MatchType selector

--- icaruscode/Utilities/IcarusObjectSelectors.cxx
+++ icaruscode/Utilities/IcarusObjectSelectors.cxx
@@ -24,7 +24,13 @@ util::MultipleChoiceSelection<MatchType> const icarus::crt::MatchTypeSelector{
   , MatchTypeSelOption_t{ MatchType::exSide           , "exSide"            }
   , MatchTypeSelOption_t{ MatchType::enTop_mult       , "enTop_mult"        }
   , MatchTypeSelOption_t{ MatchType::enTop_exSide_mult, "enTop_exSide_mult" }
+  , MatchTypeSelOption_t{ MatchType::enBottom         , "enBottom"          }
+  , MatchTypeSelOption_t{ MatchType::exBottom         , "exBottom"          }
+  , MatchTypeSelOption_t{ MatchType::enTop_exBottom   , "enTop_exBottom"    }
+  , MatchTypeSelOption_t{ MatchType::enSide_exBottom  , "enSide_exBottom"   }
+  , MatchTypeSelOption_t{ MatchType::exTop_enBottom   , "exTop_enBottom"    }
+  , MatchTypeSelOption_t{ MatchType::exSide_enBottom  , "exSide_enBottom"   }
   , MatchTypeSelOption_t{ MatchType::others           , "others"            }
-};
+  };
 
 // ---  END  ----- icarus::crt::MatchType --------------------------------------
