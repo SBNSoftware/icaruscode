73c2be12c23842d0755f6b74b2e7cb9db7ad1553
Harry Hausner <hhausner@icarusgpvm06.fnal.gov>
2023-05-24 14:22:50 -0500

    update compression handling

--- icaruscode/TPC/Compression/ProduceCompressedFragment_module.cc
+++ icaruscode/TPC/Compression/ProduceCompressedFragment_module.cc
@@ -16,6 +16,7 @@
 #include "fhiclcpp/ParameterSet.h"
 
 // C++ includes
+#include <chrono>
 #include <string>
 #include <vector>
 
@@ -63,14 +64,56 @@ namespace icarus
       if (e.getByLabel(fFragmentsLabel, uncompressedFragmentsHandle))
         art::fill_ptr_vector(uncompressedFragments, uncompressedFragmentsHandle);
 
+      // set up averages to keep track of how long the (de)compression takes and how effective it is
+      double avgCompTime   = 0;
+      double avgDecompTime = 0;
+      double avgCompSize   = 0;
+      double avgDecompSize = 0;
+
       // loop over the uncompressed fragments and make the compressed fragments
       for (auto const uncompFrag : uncompressedFragments)
       {
+        // time the creation of the compressed fragment
+        std::chrono::steady_clock::time_point compT1 = std::chrono::steady_clock::now();
         // art::Ptrs are kind of annoying
         // so to get the Fragment we need to `.get()` the Ptr and dereference
         artdaq::Fragment newFrag = icarus::PhysCrateCompressedFragment::compressArtdaqFragment(*(uncompFrag.get()));
+        std::chrono::steady_clock::time_point compT2 = std::chrono::steady_clock::now();
         producedFragments->emplace_back(newFrag);
+        double compTime = std::chrono::duration_cast<std::chrono::nanoseconds>(compT2 - compT1).count();
+        double compSize = newFrag.dataSizeBytes();
+        MF_LOG_VERBATIM("ProduceCompressedFragment")
+          << "It took "
+          << compTime
+          << " ns to compress the artdq::Fragment" << '\n'
+          << "Fragment is " << compSize << " bytes.";
+        avgCompTime += compTime;
+        avgCompSize += compSize;
+
+        // for the sake of completeness time the decompression
+        std::chrono::steady_clock::time_point decompT1 = std::chrono::steady_clock::now();
+        artdaq::Fragment decompFrag = icarus::PhysCrateCompressedFragment::decompressArtdaqFragment(newFrag);
+        std::chrono::steady_clock::time_point decompT2 = std::chrono::steady_clock::now();
+        double decompTime = std::chrono::duration_cast<std::chrono::nanoseconds>(decompT2 - decompT1).count();
+        double decompSize = decompFrag.dataSizeBytes();
+        MF_LOG_VERBATIM("ProduceCompressedFragment")
+          << "It took "
+          << decompTime
+          << " ns to decompress the artdq::Fragment" << '\n'
+          << "Fragment is " << decompSize << " bytes.";
+        avgDecompTime += decompTime;
+        avgDecompSize += decompSize;
       }
+      
+      avgCompTime   /= uncompressedFragments.size();
+      avgCompSize   /= uncompressedFragments.size();
+      avgDecompTime /= uncompressedFragments.size();
+      avgDecompSize /= uncompressedFragments.size();
+      MF_LOG_VERBATIM("ProduceCompressedFragment")
+        << "Average compression time: "   << avgCompTime << " ns"     << '\n'
+        << "Average decompression time: " << avgDecompTime << " ns"   << '\n'
+        << "Average compressed size: "    << avgCompSize << " bytes"  << '\n'
+        << "Average decompressed size: "  << avgDecompSize << " bytes";
 
       e.put(std::move(producedFragments));
     }
