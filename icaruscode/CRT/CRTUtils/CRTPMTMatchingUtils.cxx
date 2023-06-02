013d164f1833bde8edc4d0b042bdd68db7a5aa97
Anna Heggestuen <anna.heggestuen@colostate.edu>
2023-06-02 14:01:20 -0500

    add else case so no variables are left uninitialized

--- icaruscode/CRT/CRTUtils/CRTPMTMatchingUtils.cxx
+++ icaruscode/CRT/CRTUtils/CRTPMTMatchingUtils.cxx
@@ -91,6 +91,7 @@ icarus::crt::CRTMatches icarus::crt::CRTHitmatched(
     if(bottomex==0 && bottomen==0) flashType = MatchType::noMatch;
     else if (bottomex>=1 && bottomen==0) flashType = MatchType::exBottom;
     else if (bottomex==0 && bottomen>=1) flashType = MatchType::enBottom;
+    else flashType = MatchType::others;
   }
   else if (topen == 1 && sideen == 0 && topex == 0 && sideex == 0){
     if(bottomex==1 && bottomen==0) flashType = MatchType::enTop_exBottom;
