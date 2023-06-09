fca6536f4dd1b46c05c2c9a15e0ec281e6a85db2
mmrosenberg <47459703+mmrosenberg@users.noreply.github.com>
2023-06-09 12:25:15 -0400

    Merge pull request #579 from SBNSoftware/feature/aheggest_CRTPMTProd
    Put CRT-PMT Matching info in a data product

---
+++ icaruscode/Utilities/IcarusObjectSelectors.h
@@ -0,0 +1,27 @@
+/**
+ * @file   icaruscode/Utilities/IcarusObjectSelectors.h
+ * @brief  Selector implementations for some ICARUS enumerator data types.
+ * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
+ * @date   May 11, 2023
+ * @see    icaruscode/Utilities/IcarusObjectSelectors.cxx
+ */
+
+
+#ifndef ICARUSCODE_UTILITIES_ICARUSOBJECTSELECTORS_H
+#define ICARUSCODE_UTILITIES_ICARUSOBJECTSELECTORS_H
+
+
+#include "icaruscode/IcarusObj/CRTPMTMatching.h" // icarus::crt::MatchType
+#include <map> // until https://github.com/LArSoft/lardataalg/pull/41
+#include "lardataalg/Utilities/MultipleChoiceSelection.h"
+
+
+namespace icarus::crt {
+  
+  /// Selector for `icarus::crt::MatchType`.
+  extern util::MultipleChoiceSelection<MatchType> const MatchTypeSelector;
+  
+} // icarus::crt
+
+
+#endif // ICARUSCODE_UTILITIES_ICARUSOBJECTSELECTORS_H
