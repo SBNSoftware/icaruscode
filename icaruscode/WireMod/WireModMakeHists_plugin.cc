9d499b83798b9877628220bf9c4f52c74b394557
Harry Hausner <hhausner@fnal.gov>
2023-07-05 13:25:53 -0500

    get the histogram producer set up

--- icaruscode/WireMod/WireModMakeHists_plugin.cc
+++ icaruscode/WireMod/WireModMakeHists_plugin.cc
@@ -3,6 +3,7 @@
 
 //ROOT includes
 #include "TFile.h"
+#include "TH1.h"
 
 //Framework includes
 #include "art/Framework/Core/ResultsProducer.h"
@@ -17,21 +18,23 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "canvas/Utilities/InputTag.h"
 #include "lardata/Utilities/AssociationUtil.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Wire.h"
 
 namespace WireMod {
-  class MakeHistograms : public art::ResultsProducer {
+  class WireModMakeHists : public art::ResultsProducer {
   public:
-    explicit MakeHistograms(fhicl::ParameterSet const& pset);
-    ~MakeHistograms() override = default;
+    explicit WireModMakeHists(fhicl::ParameterSet const& pset);
+    ~WireModMakeHists() override = default;
     void event(art::Event const& evt) override;
     void reconfigure(fhicl::ParameterSet const& p);
     void writeResults(art::Results& r) override;
     void clear() override;
 
   private:
-    art::InputTag fTracksLabel;                // how the tracks are labeled in the input file
-    std::vecttor<art::InputTag> fHitsLabelVec; // how the hits are labeled in the input file
-  }// end MakeHistograms class
+    art::InputTag fHitsLabel; // how the hits are labeled in the input file
+  };// end WireModMakeHists class
 
   //-------------------------------------------------------------
   // Define the constructor
@@ -39,81 +42,87 @@ namespace WireMod {
   // Basically we want it to pull out the variables we defined there
   // We're doing this in the reconfigure function (because that makes it clearer what we are doing imo)
   // so just call that function
-  MakeHistograms::MakeHistograms(fhicl::ParameterSet const& pset)
+  WireModMakeHists::WireModMakeHists(fhicl::ParameterSet const& pset)
   {
     this->reconfigure(pset);
   }
 
   //-------------------------------------------------------------
   // this function pulls out the stuff we defined in the fhicl file
-  void MakeHistograms::reconfigure(fhicl::ParameterSet const& pset)
+  void WireModMakeHists::reconfigure(fhicl::ParameterSet const& pset)
   {
     // the first arguement is where in the fhicl to look, the second is the default value if that isn't found
-    fTracksLabel = pset.get<art::InputTag>("TracksLabel", "pandoraTrackGausCryoE"); // do CryoE and CryoW in fhicl
-    fHitsLabelVec = pset.get<std::vector<art::InputTag>>("HitsLabelVec"); // EE & EW or WE & WW
+    fHitsLabel= pset.get<art::InputTag>("HitsLabel", "decon1droi"); // EE & EW or WE & WW
   }
 
   //-------------------------------------------------------------
   // this function is run on every event in the art file
   // the event stores the information we want to analyze
-  void MakeHistograms::event(art::Event const& evt)
+  void WireModMakeHists::event(art::Event const& evt)
   {
-    for (auto const& fHitsLabel : fHitsLabelVec)
+    // this is what will make our histograms for us
+    art::ServiceHandle<art::TFileService>  tfs;
+
+    // get the hits out of the event and put them in a handle
+    // the handle is needed to get the associated wires
+    // we can use the handle to get a vector of hits
+    art::Handle<std::vector<recob::Hit>> hitHandle;
+    evt.getByLabel(fHitsLabel, hitHandle);
+    if (not hitHandle.isValid())
     {
-      // get the trackss out of the event and put them in a handle
-      // the handle is needed to get the associated hits
-      // we can use the handle to get a vector of tracks
-      art::Handle<std::vector<recob::Track>> trackHandle;
-      evt.getByLabel(fTracksLabel, trackHandle);
-      if (not trackHandle.valid())
+      MF_LOG_VERBATIM("WireModWireModMakeHists")
+        << "Hit handle is not valid!" << '\n'
+        << "Tried " << fHitsLabel << '\n'
+        << "abort";
+      return;
+    }
+    std::vector<art::Ptr<recob::Hit>> hitPtrVec;
+    art::fill_ptr_vector(hitPtrVec, hitHandle);
+
+    // this tool will let us get the wire for each hit
+    art::FindOneP<recob::Wire> hitToWireAssns(hitHandle, evt, fHitsLabel);
+
+    // loop over the hits
+    for (auto const& hitPtr : hitPtrVec)
+    {
+      // get info from hit here
+
+      // get the associated wire
+      art::Ptr<recob::Wire> wirePtr = hitToWireAssns.at(hitPtr.key());
+
+      // put the waveform in the histogram
+      // tfs will make a whatever is in the <>, (in this case a TH1F)
+      // the agruements past to it should be the same as for the constructor for the <object>
+      TH1F* waveformHist = tfs->make<TH1F>(("adc_"+fHitsLabel.label()+"_"+std::to_string(wirePtr.key())).c_str(), //> name of the object
+                                           ";Sample;Arbitrary Units",                                             //> axes labels
+                                           wirePtr->NSignal(),                                                    //> numbeer of bins
+                                           0,                                                                     //> lowest edge
+                                           wirePtr->NSignal());                                                   //> upper edge
+
+      // ROOT counts from 1, everyone else counts from 0
+      for (size_t bin = 1; bin < wirePtr->NSignal() + 1; ++bin)
       {
-        MF_LOG_VERBATIM("WireModMakeHistograms")
-          << "Track hand is not valid! abort";
-        return;
+        waveformHist->SetBinContent(bin, (wirePtr->Signal())[bin-1]);
       }
-      std::vector<art::Ptr<recob::Track>> trackPtrVec;
-      art::fill_ptr_vector(trackPtrVec, trackHandle);
 
-      // do a similar thing for the hits
-      art::Handle<std::vector<recob::Hit>> hitHandle;
-      evt.getByLabel(fHitsLabel, hitHandle);
-      if (not hitHandle.valid())
-      {
-        MF_LOG_VERBATIM("WireModMakeHistograms"
-          << "Hit hand is not valid! abort";
-        return;
-      }
-      std::vector<art::Ptr<recob::Hit>> hitPtrVec;
-      art::fill_ptr_vector(hitPtrVec, hitHandle)
-
-      // this tool will let us get the wire for each hit
-      art::FindOneP<recob::Wire> hitToWireAssns(hitHandle, evt, fHitsLabel);
-
-      // this one the hits for each track
-      art::FindManyP<recod::Hit> trackToHitsAssns(trackHandle, evt, fTracksLabel);
-
-      // loop over the Tracks
-      // these are art::Ptr<recob::Track>, which in most respects can be treated as a pointer to a recob::Track
-      for (auto const& trackPtr : trackPtrVec)
-      {
-        // do checks on the track here, eg is it anode/cathode crossing?
-
-        // get hits for the track
-        // the ID identifies the art::Ptr<recob::Track> to get the associated hits
-        std::vector<art::Ptr<recob::Hit>> hitsInTrack = trackToHitsAssns.at(trackPtr.ID());
-
-        // loop over the hits
-        for (auto const& hitPtr : hitsInTrack)
-        {
-          // get info from hit here
-
-          // get the associated wire
-          art::Ptr<recob::Wire> wirePtr = hitToWireAssns.at(hitPtr.ID());
-
-          // put the waveform in the histogram
-        }
-      }
+      // In testing this I just want one
+      break;
     }
   }
- 
+
+  //-------------------------------------------------------------
+  // writeResults is currently unused, but we still need to define it
+  // just have it do nothing for now
+  void WireModMakeHists::writeResults(art::Results& r)
+  {
+  }
+  
+  //-------------------------------------------------------------
+  // lear is currently unused, but we still need to define it
+  // just have it do nothing for now
+  void WireModMakeHists::clear()
+  {
+  }
+
+  DEFINE_ART_RESULTS_PLUGIN(WireModMakeHists)
 }// end WireMod namespace
