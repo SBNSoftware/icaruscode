ff92f61e7bff3d5d962979e7cd657614cac4c5ec
Bruce Howard <howard@fnal.gov>
2024-02-15 23:34:45 -0600

    Commit updated service put together by Jaesung and I so that when one makes stage1 aware of the trigger emulation, Pandora will see them shifted a bit in X to hopefully get closer to where they actually belong. Trying with this commit to put the relevant actions in icaruscode for a patch.

---
+++ icaruscode/TPC/Utilities/DetectorPropertiesServiceICARUSClockOffsetMC_service.cc
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  \file DetectorPropertiesServiceICARUSClockOffsetMC_service.cc
+//
+////////////////////////////////////////////////////////////////////////
+
+// icaruscode includes
+#include "icaruscode/TPC/Utilities/DetectorPropertiesServiceICARUSClockOffsetMC.h"
+
+// Art includes
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+
+DEFINE_ART_SERVICE_INTERFACE_IMPL(detinfo::DetectorPropertiesServiceICARUSClockOffsetMC,
+                                  detinfo::DetectorPropertiesService)
