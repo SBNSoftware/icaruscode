8456f718a0550d7613df17dfe7801d5b2b5b66d3
Gianluca Petrillo <petrillo@slac.stanford.edu>
2024-02-20 20:35:39 -0600

    Decoder channel mapping: added interface documentation and full mapping dump
    Except for CRT calibration information.

--- icaruscode/Decode/ChannelMapping/IICARUSChannelMap.h
+++ icaruscode/Decode/ChannelMapping/IICARUSChannelMap.h
@@ -1,23 +1,20 @@
-///////////////////////////////////////////////////////////////////////
-///
-/// \file   IICARUSChannelMap
-///
-/// \brief  Interface class for hardware/software channel mapping 
-///         for ICARUS
-///
-/// \author T. Usher
-///
-////////////////////////////////////////////////////////////////////////
+/**
+ * @file   icaruscode/Decode/ChannelMapping/IICARUSChannelMap.h
+ * @brief  Interface class for hardware/software channel mapping for ICARUS.
+ * @author T. Usher
+ */
 
-#ifndef IICARUSChannelMap_H
-#define IICARUSChannelMap_H
+#ifndef ICARUSCODE_DECODE_CHANNELMAPPING_IICARUSCHANNELMAP_H
+#define ICARUSCODE_DECODE_CHANNELMAPPING_IICARUSCHANNELMAP_H
 
 #include "icaruscode/Decode/ChannelMapping/RunPeriods.h"
 
 #include "art/Framework/Services/Registry/ServiceDeclarationMacros.h"
 
 #include <vector>
+#include <map>
 #include <string>
+#include <tuple>
 
 namespace icarusDB {
 
@@ -39,41 +36,110 @@ public:
     virtual ~IICARUSChannelMap() noexcept = default;
     
     
+    /// --- BEGIN --- Data period selection ------------------------------------
+    /// @name Data period selection
+    /// @{
+    
     /// Loads the mapping for `run`, returns whether a new mapping was loaded.
     virtual bool                                    forRun(int run)                               = 0;
     
     /// Loads the mapping for `period`, returns whether a new mapping was loaded.
     virtual bool                                    forPeriod(icarusDB::RunPeriod period)         = 0;
     
-    // Section to access fragment to board mapping
-    virtual bool                                    hasFragmentID(const unsigned int)       const = 0;
+    /// @}
+    /// --- END ----- Data period selection ------------------------------------
+    
+    
+    /// --- BEGIN --- TPC information ------------------------------------------
+    /// @name TPC information
+    /// @{
+    
+    /// --- BEGIN - - TPC fragment information - - - - - - - - - - - - - - - - -
+    /// @name TPC fragment information
+    /// @{
+    
+    /// Returns whether the specified `ID` is a known TPC fragment ID.
+    virtual bool                                    hasFragmentID(const unsigned int ID)       const = 0;
     /// Returns the number of TPC fragment IDs known to the service.
-    virtual unsigned int                            nTPCfragmentIDs() const = 0;
-    virtual const std::string&                      getCrateName(const unsigned int)        const = 0;
-    virtual const ReadoutIDVec&                     getReadoutBoardVec(const unsigned int)  const = 0;
-    virtual const TPCReadoutBoardToChannelMap&      getReadoutBoardToChannelMap()           const = 0;
 
-    // Section to access channel information for a given board
-    virtual bool                                    hasBoardID(const unsigned int)          const = 0;
+    /// Returns the number of known TPC fragments.
+    virtual unsigned int                            nTPCfragmentIDs() const = 0;
+    
+    /// Returns the name of the crate served by the specified `fragmentID`.
+    virtual const std::string&                      getCrateName(const unsigned int fragmentID) const = 0;
+    
+    /// Returns the list of board IDs included in the specified `fragmentID`.
+    virtual const ReadoutIDVec&                     getReadoutBoardVec(const unsigned int fragmentID) const = 0;
+    
+    /// @}
+    /// --- END - - - TPC fragment information - - - - - - - - - - - - - - - - -
+    
+    /**
+     * @brief Returns the full TPC channel mapping.
+     * 
+     * The returned mapping is an associative container, associating each board
+     * ID (`boardID`) to the following information (in a `std::tuple`):
+     *  * `[0]` slot number the board is on (like `getBoardSlot(boardID)`)
+     *  * `[1]` list of channels served by the board, and their plane
+     *          (like `getChannelPlanePair(boardID)`)
+     * 
+     */
+    virtual const TPCReadoutBoardToChannelMap&      getReadoutBoardToChannelMap()           const = 0;
+    
+    /// --- BEGIN - - TPC board information  - - - - - - - - - - - - - - - - - -
+    /// @name TPC board information
+    /// @{
+    
+    /// Returns whether there is a board with the specified `ID`.
+    virtual bool                                    hasBoardID(const unsigned int ID)          const = 0;
     /// Returns the number of TPC board IDs known to the service.
     virtual unsigned int                            nTPCboardIDs() const = 0;
-    virtual unsigned int                            getBoardSlot(const unsigned int)        const = 0;
-    virtual const ChannelPlanePairVec&              getChannelPlanePair(const unsigned int) const = 0;
+    /// Returns the number of slot the `boardID` is on.
+    virtual unsigned int                            getBoardSlot(const unsigned int boardID) const = 0;
+    /// Returns a list of channels served by the `boardID` and for each the plane it is on (`0` to `2`).
+    virtual const ChannelPlanePairVec&              getChannelPlanePair(const unsigned int boardID) const = 0;
 
-    // Section for recovering PMT information
-    virtual bool                                    hasPMTDigitizerID(const unsigned int)   const = 0;
-    /// Returns the number of PMT fragment IDs known to the service.
+    /// @}
+    /// --- END - - - TPC board information  - - - - - - - - - - - - - - - - - -
+    
+    
+    /// --- BEGIN --- PMT information ------------------------------------------
+    /// @name PMT information
+    /// @{
+    
+    /// Returns whether the specified fragment `ID` is known to the mapping.
+    virtual bool                                    hasPMTDigitizerID(const unsigned int ID) const = 0;
+    
+    /// Returns the number of PMT fragment IDs known to the mapping.
     virtual unsigned int                            nPMTfragmentIDs() const = 0;
+    
+    /// Returns a list of triplets: 
     virtual const DigitizerChannelChannelIDPairVec& getChannelIDPairVec(const unsigned int) const = 0;
+    
+    /// @}
+    /// --- END ----- PMT information ------------------------------------------
 
-    virtual unsigned int                            getSimMacAddress(const unsigned int)    const = 0;
+
+    /// --- BEGIN --- CRT information ------------------------------------------
+    /// @name CRT information
+    /// @{
+    
+    /// Returns the sim Mac address corresponding to the specified side CRT hardware address.
+    virtual unsigned int                            getSimMacAddress(const unsigned int hwmacaddress)    const = 0;
+    /// Returns the sim Mac address corresponding to the specified top CRT hardware address.
     virtual unsigned int                         gettopSimMacAddress(const unsigned int)    const = 0;
 
+    /// Returns the Gain and Pedestal for Side CRT.
     virtual std::pair<double, double>          getSideCRTCalibrationMap(int mac5, int chan) const = 0;
-};
+    
+    /// @}
+    /// --- END ----- CRT information ------------------------------------------
 
-} // end of namespace
+}; // IICARUSChannelMap
+
+} // namespace icarusDB
 
 DECLARE_ART_SERVICE_INTERFACE(icarusDB::IICARUSChannelMap, SHARED)
 
-#endif
+
+#endif // ICARUSCODE_DECODE_CHANNELMAPPING_IICARUSCHANNELMAP_H
--- icaruscode/Decode/ChannelMapping/PMTChannelMapDumper.cxx
+++ icaruscode/Decode/ChannelMapping/PMTChannelMapDumper.cxx
@@ -4,7 +4,7 @@
  * @author Gianluca Petrillo (petrillo@slac.stanford.edu)
  * 
  * This utility can be run with any configuration file including a configuration
- * for `IICARUSChannel` service.
+ * for `IICARUSChannelMap` service.
  * 
  * It is using _art_ facilities for tool loading, but it does not run in _art_
  * environment. So it may break without warning and without solution.
@@ -18,6 +18,7 @@
 
 // LArSoft and framework libraries
 #include "larcorealg/CoreUtils/enumerate.h"
+#include "larcorealg/CoreUtils/counter.h"
 #include "larcorealg/TestUtils/unit_test_base.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
@@ -48,49 +49,194 @@ struct SortByElement {
 
 
 // -----------------------------------------------------------------------------
-void dumpMapping(icarusDB::ICARUSChannelMapProvider const& channelMapping) {
+/// Helper to split text in rows.
+class Pager {
+  unsigned int fLinesPerPage;
+  
+  unsigned int fLinesLeft = 1;
+  
+    public:
+  Pager(unsigned int linesPerPage): fLinesPerPage{ linesPerPage } {}
+  
+  /// Increments the number of line and says if it starts on a new page.
+  bool nextHasNewLine()
+    {
+      if (--fLinesLeft) return false;
+      fLinesLeft = fLinesPerPage;
+      return true;
+    }
+  bool operator() () { return nextHasNewLine(); }
+  
+}; // Pager
+
+
+// -----------------------------------------------------------------------------
+void dumpTPCmapping(icarusDB::ICARUSChannelMapProvider const& mapping) {
+  
+  // TODO
+  
+  constexpr unsigned int FragmentIDoffset = 0x1000;
+  
+  mf::LogVerbatim log("PMTchannelMappingDumper");
+  
+  // by fragment
+  unsigned int nFragmentIDs = mapping.nTPCfragmentIDs();
+  log << "Reported " << nFragmentIDs << " TPC fragment IDs.";
+  unsigned int nFragmentsFound = 0;
+  for (
+    unsigned int fragmentID = FragmentIDoffset;
+    nFragmentsFound < nFragmentIDs;
+    ++fragmentID
+  ) {
+    
+    if (!mapping.hasFragmentID(fragmentID)) continue;
+    ++nFragmentsFound;
+    
+    icarusDB::ReadoutIDVec const& boardIDs
+      = mapping.getReadoutBoardVec(fragmentID);
+      
+    log << "\nFragment ID 0x" << std::hex << fragmentID << std::dec
+      << " on crate " << mapping.getCrateName(fragmentID) << " has "
+      << boardIDs.size() << " boards:";
+    for (unsigned int boardID: boardIDs) log << " " << boardID;
+    
+  } // for fragment
+  
+  // by board
+  unsigned int const nBoards = mapping.nTPCboardIDs();
+  log << "\n\nReported " << nBoards << " TPC readout board IDs.";
+  unsigned int nFoundBoards = 0;
+  for (unsigned int boardID = 0; nFoundBoards < nBoards; ++boardID) {
+    
+    if (!mapping.hasBoardID(boardID)) continue;
+    ++nFoundBoards;
+
+    /// Returns the number of TPC board IDs known to the service.
+    unsigned int const slot = mapping.getBoardSlot(boardID);
+    
+    icarusDB::ChannelPlanePairVec const& channelPlanes
+      = mapping.getChannelPlanePair(boardID);
+    log << "\nBoard " << boardID << " on slot #" << slot << " has "
+      << channelPlanes.size() << " channels:";
+    Pager pager{ 4 };
+    for (auto [ channel, plane ]: channelPlanes) {
+      if (pager.nextHasNewLine()) log << "\n     ";
+      log << " CH=" << channel << " (P:" << plane << ")";
+    }
+    
+  } // for board
+
+  // full mapping
+  icarusDB::TPCReadoutBoardToChannelMap const& boardChannelMap
+    = mapping.getReadoutBoardToChannelMap(); // map<board, SlotChannelVecPair>
+  log << "\n\nReported " << nBoards << " TPC boards in the mapping:";
+  
+  // board (unsigned int), SlotChannelVecPair = pair<slot, channels>:
+  for (auto const& [ board, slotInfo ]: boardChannelMap) {
+    
+    auto const& [ slot, channelPlanes ] = slotInfo;
+    
+    log << "\nBoard " << board << " on slot #" << slot
+      << " has " << channelPlanes.size() << " channels:";
+    
+    Pager pager{ 4 };
+    for (auto [ channel, plane ]: channelPlanes) {
+      if (pager.nextHasNewLine()) log << "\n     ";
+      log << " CH=" << channel << " (P:" << plane << ")";
+    }
+    
+  } // for board channel map
+  
+} // dumpTPCmapping()
+
+
+// -----------------------------------------------------------------------------
+void dumpPMTmapping(icarusDB::ICARUSChannelMapProvider const& mapping) {
+  
+  mf::LogVerbatim log("PMTchannelMappingDumper");
+  unsigned int nFragmentIDs = mapping.nPMTfragmentIDs();
+  log << "Reported " << nFragmentIDs << " PMT fragment IDs.";
   
   // hard-coded list of fragment ID; don't like it?
   // ask for an extension of the channel mapping features.
   std::array<unsigned int, 24U> FragmentIDs;
   std::iota(FragmentIDs.begin(), FragmentIDs.end(), 0x2000);
   
-  mf::LogVerbatim("PMTchannelMappingDumper") << "Fragment IDs:";
+  log << "\nPMT fragment IDs:";
   for (auto const [ iFragment, fragmentID]: util::enumerate(FragmentIDs)) {
     unsigned int const effFragmentID = fragmentID & 0xFF;
     
-    if (!channelMapping.hasPMTDigitizerID(effFragmentID)) {
-      mf::LogVerbatim("PMTchannelMappingDumper")
-        << "[" << iFragment << "] " << std::hex << fragmentID << std::dec
+    if (!mapping.hasPMTDigitizerID(effFragmentID)) {
+      log << "\n[" << iFragment << "] " << std::hex << fragmentID << std::dec
         << " not found in the database (as "
         << std::hex << effFragmentID << std::dec << ")";
       continue;
     }
     
     icarusDB::DigitizerChannelChannelIDPairVec digitizerChannels
-      = channelMapping.getChannelIDPairVec(effFragmentID);
+      = mapping.getChannelIDPairVec(effFragmentID);
     
     std::sort
       (digitizerChannels.begin(), digitizerChannels.end(), SortByElement<1U>{});
     
     
-    mf::LogVerbatim log("PMTchannelMappingDumper");
     log
-      << "[" << iFragment << "] " << std::hex << fragmentID << std::dec
+      << "\n[" << iFragment << "] 0x" << std::hex << fragmentID << std::dec
       << " includes " << digitizerChannels.size()
       << " LArSoft channels between " << std::get<1U>( digitizerChannels.front() )
       << " and " << std::get<1U>( digitizerChannels.back() )
       << " [board channel index in brackets]:";
-    constexpr unsigned int Cols = 8U;
-    unsigned int n = 0;
+    Pager pager{ 8 };
     for(auto const & [ digitizerChannel, channelID, laserChannel ]: digitizerChannels) {
-      if (n-- == 0) { log << "\n     "; n = Cols - 1U; }
+      if (pager.nextHasNewLine()) log << "\n     ";
       log << " "  << std::setw(3) << channelID
         << " [" << std::setw(3) << digitizerChannel << "]";
     } // for channel
     
   } // for fragment
   
+} // dumpPMTmapping()
+
+
+// -----------------------------------------------------------------------------
+void dumpCRTmapping(icarusDB::ICARUSChannelMapProvider const& mapping) {
+  
+  mf::LogVerbatim log("PMTchannelMappingDumper");
+  
+  constexpr unsigned int MaxHWmac = 1000;
+  
+  log << "CRT address mapping:";
+  for (unsigned int const HWaddress: util::counter(MaxHWmac)) {
+    unsigned int const simMac = mapping.getSimMacAddress(HWaddress);
+    if (simMac == 0) continue; // not present
+    log << "\nSide CRT HW=" << HWaddress << " -> sim=" << simMac;
+  }
+  for (unsigned int const HWaddress: util::counter(MaxHWmac)) {
+    unsigned int const simMac = mapping.gettopSimMacAddress(HWaddress);
+    if (simMac == 0) continue; // not present
+    log << "\nTop CRT  HW=" << HWaddress << " -> sim=" << simMac;
+  }
+  
+  // the interface does not provide any clue on which mac5 and chan values are
+  // available, so we have to skip this
+  // std::pair<double, double> const = mapping.getSideCRTCalibrationMap(mac5, chan);
+   
+} // dumpCRTmapping()
+
+
+// -----------------------------------------------------------------------------
+void dumpMapping(icarusDB::ICARUSChannelMapProvider const& channelMapping) {
+  
+  mf::LogVerbatim("PMTchannelMappingDumper") << std::string(80, '-');
+  dumpTPCmapping(channelMapping);
+  
+  mf::LogVerbatim("PMTchannelMappingDumper") << std::string(80, '-');
+  dumpPMTmapping(channelMapping);
+  
+  mf::LogVerbatim("PMTchannelMappingDumper") << std::string(80, '-');
+  dumpCRTmapping(channelMapping);
+  
+  mf::LogVerbatim("PMTchannelMappingDumper") << std::string(80, '-');
 }
 
 
