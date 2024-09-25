999135419ea4216b77ef4da7fd2b3d95edeceae7
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-09-25 12:01:42 -0500

    add more info in comments

--- icaruscode/IcarusObj/PMTBeamSignal.h
+++ icaruscode/IcarusObj/PMTBeamSignal.h
@@ -16,32 +16,48 @@
 namespace icarus::timing
 {
 
-  /// Special value to denote no special channel information
+  /// Special value to denote no special channel information.
   static constexpr auto NoChannel = std::numeric_limits<unsigned int>::max();
-  /// Special value to denote no time channel informatio
+  /// Special value to denote no time channel information.
   static constexpr double NoTime = 0.;
-  // Special value to denote no sample information
+  // Special value to denote no sample information.
   static constexpr std::size_t NoSample = 0;
+
+  /**
+   * @brief Beam time as seen by a PMT readout board.
+   *
+   * This could either be an early warning (EW) or a resistive wall monitor (RWM) time.
+   * These signals are delivered via fibers and digitized in special PMT channels.
+   *
+   * Both the time in @ref DetectorClocksElectronicsTime "electronics time scale"
+   * and time time relative to the hardware trigger are included.
+   *
+   * The information in this object may be missing: its validity should
+   * always be checked in advance with `isValid()`.
+   */
 
   struct PMTBeamSignal
   {
 
-    /// Special channel this time was extracted from
+    /// Special channel this time was extracted from.
+    /// These are defined in `CAEN_V1730_setup_icarus.fcl`.
     unsigned int specialChannel = NoChannel;
 
-    /// Board on which the special channel is on
+    /// Board on which the special channel is on (e.g: WW-TOP-A).
+    /// Should match the same format as `icarusDB::PMTChannelInfo_t::digitizerLabel`.
     std::string digitizerLabel = "";
 
-    /// Crate this time applies to
+    /// Crate this time applies to (e.g.: WW-TOP).
+    /// Corresponds to the first part of `digitizerLabel`.
     std::string crate = "";
 
-    /// Sample within the waveform where the reference signal is found
+    /// Sample within the waveform where the reference signal is found.
     std::size_t sample = NoSample;
 
-    /// Start time in electronics time [us]
+    /// Start time in electronics time [us].
     double startTimeAbs = NoTime;
 
-    /// Start time relative to trigger time [us]
+    /// Start time relative to trigger time [us].
     double startTime = NoTime;
 
     PMTBeamSignal(unsigned int ch, std::string b, std::string c,
