6f211ceeb85bd9b33e65adfa49d696fa8bb7a3ee
Giuseppe Cerati <cerati@fnal.gov>
2025-04-10 17:15:05 -0500

    add script to start local triton server

---
+++ icaruscode/NuGraphIcarus/scripts/setup_tritonservier-nugraph-v0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+unset PYTHONHOME
+unset PYTHONPATH
+rm -f tritonserver_nugraph-v0.log
+export APPTAINER_BIND=/etc/hosts,/tmp,/cvmfs
+/cvmfs/oasis.opensciencegrid.org/mis/apptainer/1.3.2/bin/apptainer run --pid --ipc --home ~/:${HOME} --pwd ${PWD} /cvmfs/icarus.opensciencegrid.org/containers/tritonserver/nugraph-v0/ >& tritonserver_nugraph-v0.log &
