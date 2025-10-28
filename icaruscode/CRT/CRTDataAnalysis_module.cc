d73ef52b4fc16ee521db73f8420c30a17508e093
Francesco Poppi <poppi@bo.infn.it>
2025-10-28 22:58:49 +0100

    BugFix: If CRTData are not present, hitTreeun number and subRun number were left uninitialized.

--- icaruscode/CRT/CRTDataAnalysis_module.cc
+++ icaruscode/CRT/CRTDataAnalysis_module.cc
@@ -209,6 +209,8 @@ namespace crt {
     vector<vector<int>> fDetPDG; /// signal inducing particle(s)' PDG code
 
     //CRT hit product vars
+    int      fHitRun;
+    int      fHitSubRun;
     int      fHitEvent;
     float    fXHit; ///< reconstructed X position of CRT hit (cm)
     float    fYHit; ///< reconstructed Y position of CRT hit (cm)
@@ -373,8 +375,8 @@ namespace crt {
     fDAQNtuple->Branch("gate_start_timestamp", &m_gate_start_timestamp, "gate_start_timestamp/l");
 
     // Define the branches of our SimHit n-tuple
-    fHitNtuple->Branch("run",         &fDetRun,      "run/I");
-    fHitNtuple->Branch("subrun",      &fDetSubRun,   "subrun/I");
+    fHitNtuple->Branch("run",         &fHitRun,      "run/I");
+    fHitNtuple->Branch("subrun",      &fHitSubRun,   "subrun/I");
     fHitNtuple->Branch("event",       &fHitEvent,    "event/I");
     fHitNtuple->Branch("nHit",        &fNHit,        "nHit/I");
     fHitNtuple->Branch("x",           &fXHit,        "x/F");
@@ -578,6 +580,8 @@ namespace crt {
       for ( auto const& hit : *crtHitHandle )
         {
 	  fNHit++;
+    fHitRun = fRun;
+    fHitSubRun = fSubRun;
 	  fHitEvent = fEvent;
 	  fXHit    = hit.x_pos;
 	  fYHit    = hit.y_pos;
