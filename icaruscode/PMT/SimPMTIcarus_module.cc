8b09af5d7c29c5a12554f34ff8ad1da3dc71a3e2
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2026-03-05 11:59:37 -0500

    fixes + indentation

--- icaruscode/PMT/SimPMTIcarus_module.cc
+++ icaruscode/PMT/SimPMTIcarus_module.cc
@@ -281,6 +281,7 @@ namespace icarus::opdet {
     
     bool fMakeMetadata; ///< Whether to produce waveform metadata.
     bool fWritePhotons { false }; ///< Whether to save contributing photons.
+    bool fDoTimingDelays; ///< Whether timing delay corrections are applied.
 
     CLHEP::HepRandomEngine&  fEfficiencyEngine;
     CLHEP::HepRandomEngine&  fDarkNoiseEngine;
@@ -351,6 +352,7 @@ SimPMTIcarus::SimPMTIcarus(Parameters const& config)
     , fInputModuleName(config().inputModuleLabel())
     , fMakeMetadata(config().MakeMetadata())
     , fWritePhotons(config().writePhotons())
+    , fDoTimingDelays(config().algoConfig().ApplyTimingDelays())
     // random engines
     , fEfficiencyEngine(art::ServiceHandle<rndm::NuRandomService>()->registerAndSeedEngine(
           createEngine(0, "HepJamesRandom", "Efficiencies"),
@@ -460,7 +462,7 @@ SimPMTIcarus::SimPMTIcarus(Parameters const& config)
       e.time().value(), // using the event generation time as beam time stamp
       *(lar::providerFrom<detinfo::LArPropertiesService>()),
       clockData,
-      *(lar::providerFrom<icarusDB::IPMTTimingCorrectionService>()),
+      fDoTimingDelays ? lar::providerFrom<icarusDB::IPMTTimingCorrectionService>() : nullptr,
       *fSinglePhotonResponseFunc,
       *fPedestalGen,
       fEfficiencyEngine,
